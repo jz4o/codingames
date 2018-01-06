6467dc4af97cb0997700b7be6e1d42c423bab1db
jz4o <26317360+jz4o@users.noreply.github.com>
2018-01-07 01:54:31 +0900

    ONEWAY CITY を追加

---
+++ practice/community_puzzle/oneway-city.rb
@@ -0,0 +1,21 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@m = gets.to_i
+@n = gets.to_i
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+distances = [@m - 1, @n - 1]
+
+# one way city
+(puts 1; return) if distances.min <= 0
+
+mole_combination = ((distances.max + 1)..distances.sum).inject { |n, sum| sum * n }
+deno_combination = (1..distances.min).inject{ |n, sum| sum * n }
+
+movable_combination = mole_combination / deno_combination
+significant_digits  = movable_combination.to_s.[](0...1000)
+
+puts significant_digits
