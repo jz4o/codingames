dcd7cccce048aebf400f32cf03fe1f89ac7a0d81
jz4o <26317360+jz4o@users.noreply.github.com>
2018-04-10 23:44:04 +0900

    THE HUNGRY DUCK - PART 1 を追加

---
+++ practice/community_puzzle/the-hungry-duck---part-1.rb
@@ -0,0 +1,29 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@w, @h = gets.split(" ").collect {|x| x.to_i}
+map = Array.new(@h){ Array.new(@w){ nil } }
+@h.times do |h|
+  inputs = gets.split(" ")
+  for j in 0..(@w-1)
+    food = inputs[j].to_i
+    map[h][j] = food
+  end
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+@h.times do |height|
+  @w.times do |width|
+    top  = height.zero? ? 0 : map[height-1][width]
+    left = width.zero?  ? 0 : map[height][width-1]
+
+    map[height][width] += [top, left].max
+  end
+end
+
+puts map.last.last
+
