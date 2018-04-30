8ff936369baf99fc940d0d4343e59fef23b7e0f7
jz4o <26317360+jz4o@users.noreply.github.com>
2018-04-30 17:56:24 +0900

    COUNT OF PRIMES IN A NUMBER GRID を追加

---
+++ practice/community_puzzle/count-of-primes-in-a-number-grid.rb
@@ -0,0 +1,29 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@r, @c = gets.split(" ").collect {|x| x.to_i}
+digit_map = []
+@r.times do
+  digit_map << gets.chomp.split
+end
+
+require 'prime'
+
+def combination_digits(digits_row)
+  result = []
+  (2..digits_row.length).each do |i|
+    digits_row.each_cons(i) do |digits|
+      result << digits.join.to_i
+    end
+  end
+  result
+end
+
+digits = []
+digits << digit_map.flatten.map(&:to_i)
+digits << digit_map.map { |row| combination_digits row }
+digits << digit_map.transpose.map { |row| combination_digits row }
+digits.flatten!.uniq!
+
+puts digits.select { |digit| digit.prime? }.size
+
