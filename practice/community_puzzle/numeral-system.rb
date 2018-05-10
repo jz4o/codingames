b6ba8dc6f2747f08ef178dff92eba326c17a6b4d
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-10 23:46:25 +0900

    NUMERAL SYSTEM を追加

---
+++ practice/community_puzzle/numeral-system.rb
@@ -0,0 +1,22 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@equality = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+NUMERALS = [*'0'..'9', *'A'..'Z']
+
+x, y, z = @equality.split(/[\+=]/)
+
+equality_chars = @equality.chars
+max_numeral = NUMERALS.select { |n| equality_chars.include? n }.last
+minimum_n   = NUMERALS.index(max_numeral).next
+
+(minimum_n..NUMERALS.size).each do |i|
+  p i and break if x.to_i(i) + y.to_i(i) == z.to_i(i)
+end
+
