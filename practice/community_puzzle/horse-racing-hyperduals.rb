39c114523dd23c94a7b95846df4f3ffe98b8ba2d
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-13 13:00:04 +0900

    HORSE-RACING HYPERDUALS を追加

---
+++ practice/community_puzzle/horse-racing-hyperduals.rb
@@ -0,0 +1,21 @@
+uto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+strengths = []
+@n.times do
+  v, e = gets.split(" ").collect {|x| x.to_i}
+  strengths << { v: v, e: e }
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "42"
+
+puts strengths.flat_map.with_index(1) { |left, index|
+  strengths[index..-1].map do |right|
+    (right[:v] - left[:v]).abs + (right[:e] - left[:e]).abs
+  end
+}.reject(&:negative?).min
+
