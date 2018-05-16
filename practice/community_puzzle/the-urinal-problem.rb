c718dd4bef1ff94478ccdcdf229e3b3699ab5e80
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-17 00:31:30 +0900

    THE URINAL PROBLEM を追加

---
+++ practice/community_puzzle/the-urinal-problem.rb
@@ -0,0 +1,22 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+@b = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "0"
+
+free_indexes = *0...@n
+go_right_indexes = go_left_indexes = @b.chars.map.with_index { |b, i| i if b == '!' }.compact
+
+while 1 < free_indexes.size do
+  free_indexes -= (go_right_indexes + go_left_indexes)
+  go_left_indexes = go_left_indexes.map(&:pred)
+  go_right_indexes = go_right_indexes.map(&:next)
+end
+
+puts free_indexes.pop
+
