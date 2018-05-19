0846b9399783899276ccb20f1c4312e021d5be7b
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-19 11:56:37 +0900

    MICRO ASSEMBLY を追加

---
+++ practice/community_puzzle/micro-assembly.rb
@@ -0,0 +1,33 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a, b, c, d = gets.split(" ").collect {|x| x.to_i}
+@n = gets.to_i
+instructions = []
+@n.times do
+  instructions << gets.chomp
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts a b c d"
+
+index = 0
+while index < instructions.size do
+  operator, *args = instructions[index].split
+
+  # 加算による減算を行うため正負反転
+  args[2].prepend '-' if operator == 'SUB'
+
+  if operator == 'JNE'
+    index = args[0].to_i and next if instance_eval [args[1], '!=', args[2]].join
+  else
+    instance_eval [args[0], '=', args[1], '+', args[2] || 0].join
+  end
+
+  index = index.next
+end
+
+puts [a, b, c, d].join(' ')
+
