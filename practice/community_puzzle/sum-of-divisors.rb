b49c5a926e9ea7ead2dcff80cc36648f6c056d56
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-22 23:26:41 +0900

    SUM OF DIVISORS を追加

---
+++ practice/community_puzzle/sum-of-divisors.rb
@@ -0,0 +1,12 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "sum"
+
+puts (1..@n).map { |n| (@n / n) * n }.sum
+
