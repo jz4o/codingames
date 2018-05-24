4cf4ea8783bdb97d135a9acd613b8ff00fba4e12
jz4o <26317360+jz4o@users.noreply.github.com>
2018-05-24 23:05:38 +0900

    EGYPTIAN MULTIPLICATION を追加

---
+++ practice/community_puzzle/egyptian-multiplication.rb
@@ -0,0 +1,33 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@a, @b = gets.split(" ").collect {|x| x.to_i}
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+high = [@a, @b].max
+low = @a + @b - high
+
+# initial formula
+puts "#{high} * #{low}"
+
+add_nums = ""
+until low.zero? do
+  if low.even?
+    high *= 2
+    low /= 2
+  else
+    add_nums << " + #{high}"
+    low = low.pred
+  end
+
+  # way formula
+  puts "= #{high} * #{low}#{add_nums}"
+end
+
+# answer
+puts "= #{@a * @b}"
+
