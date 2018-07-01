99b9023e7c8583030263caeacb0d8186abba5859
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-01 22:05:07 +0900

    SELF-DRIVING CAR TESTING を追加

---
+++ practice/community_puzzle/self-driving-car-testing.rb
@@ -0,0 +1,30 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+@position, *commands = gets.chomp.split(';')
+@position = @position.to_i
+@commands = commands.flat_map do |command|
+  time, comm = command[0..-2], command[-1]
+  (comm * time.to_i).chars
+end
+
+@n.times do |n|
+  count, pattern = gets.chomp.split(';')
+  count.to_i.times do
+    case @commands.shift
+    when 'L' then @position -= 1
+    when 'R' then @position += 1
+    end
+
+    output_pattern = pattern.dup
+    output_pattern[@position - 1 ] = '#'
+    puts output_pattern
+  end
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
