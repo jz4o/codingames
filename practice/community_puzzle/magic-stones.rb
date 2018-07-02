dd49371ec607231327d224cd93e28a1e0bf5a8e6
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-02 23:12:24 +0900

    MAGIC STONES を追加

---
+++ practice/community_puzzle/magic-stones.rb
@@ -0,0 +1,31 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+inputs = gets.split(" ")
+stones = []
+for i in 0..(@n-1)
+  stones << inputs[i].to_i
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "1"
+
+stone_count = 0
+levels = stones.uniq.sort
+while level = levels.shift
+  target_stone_count = stones.count level
+  next_level_stone_count, target_stone_count = target_stone_count.divmod 2
+  stones.delete level
+
+  unless next_level_stone_count.zero?
+    stones += [level.next] * next_level_stone_count
+    levels.unshift level.next
+  end
+  stone_count += target_stone_count
+end
+
+puts stone_count
+
