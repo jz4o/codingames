04e758aea07e269410fbcbb7daca839140279343
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-03 23:03:26 +0900

    BANK ROBBERS を追加

---
+++ practice/community_puzzle/bank-robbers.rb
@@ -0,0 +1,18 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@r = gets.to_i
+@v = gets.to_i
+robbers = Array.new(@r, 0)
+@v.times do
+  c, n = gets.split(" ").collect {|x| x.to_i}
+  vault_time = (10 ** n) * (5 ** (c - n))
+  robbers[robbers.index(robbers.min)] += vault_time
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "1"
+puts robbers.max
+
