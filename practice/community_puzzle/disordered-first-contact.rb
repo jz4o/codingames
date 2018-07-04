7b8115dd9b97e3a48b943e731f8690cbaae2f6ac
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-04 22:55:19 +0900

    DISORDERED FIRST CONTACT を追加

---
+++ practice/community_puzzle/disordered-first-contact.rb
@@ -0,0 +1,50 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+@message = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+def decode(text)
+  result = ''
+  turn = 1
+  while (1..turn).sum < text.size
+    turn += 1
+  end
+
+  while turn.positive?
+    slice_size = text.size - (1..turn.pred).sum
+    if turn.odd?
+      result.insert 0, text.slice!(-slice_size..-1)
+    else
+      result.insert 0, text.slice!(0, slice_size)
+    end
+    turn -= 1
+  end
+
+  result
+end
+
+def encode(text)
+  result = ''
+  turn = 1
+  until (target = text.slice!(0, turn)).empty?
+    if turn.odd?
+      result << target
+    else
+      result.insert 0, target
+    end
+    turn += 1
+  end
+
+  result
+end
+
+decode_or_encode = method(@n.positive? ? :decode : :encode)
+@n.abs.times { @message = decode_or_encode.call(@message) }
+puts @message
+
