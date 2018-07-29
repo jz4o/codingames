917a054ea64201dcc6c9f700209f9e0929254abf
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-30 00:26:58 +0900

    GRAVITY TUMBLER を追加

---
+++ practice/community_puzzle/gravity-tumbler.rb
@@ -0,0 +1,22 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@width, @height = gets.split(" ").collect {|x| x.to_i}
+@count = gets.to_i
+rasters = []
+@height.times do
+    raster = gets.chomp
+    rasters << raster.chars
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "..."
+# puts "write ###"
+
+@count.times do
+    rasters = rasters.map(&:sort).transpose.reverse
+end
+puts rasters.map(&:join)
+
