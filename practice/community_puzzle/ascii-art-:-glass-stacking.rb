a9742b08300aa1b873a7435008398310b38522fa
jz4o <26317360+jz4o@users.noreply.github.com>
2018-09-23 22:05:23 +0900

    ASCII ART : GLASS STACKING を追加

---
+++ practice/community_puzzle/ascii-art-:-glass-stacking.rb
@@ -0,0 +1,29 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+stage = 0
+while @n >= stage.next
+  @n -= stage += 1
+end
+
+GLASS_PARTS = [
+ ' *** ',
+ ' * * ',
+ ' * * ',
+ '*****'
+]
+WIDTH = stage * GLASS_PARTS.first.size + stage.pred
+
+(1..stage).each do |step|
+  GLASS_PARTS.each do |glass_part|
+    puts [glass_part].*(step).join(' ').center(WIDTH)
+  end
+end
+
