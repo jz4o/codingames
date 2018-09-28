7e1d855866f6c0ea8c42d5234cb116c482f4355d
jz4o <26317360+jz4o@users.noreply.github.com>
2018-09-28 22:08:38 +0900

    FORMAT STRING VALIDATION を追加

---
+++ practice/community_puzzle/format-string-validation.rb
@@ -0,0 +1,13 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@text = gets.chomp
+@format = gets.chomp
+
+@format.gsub! '\\', '\\\\\\'
+%w|^ * ( ) [ ] $|.each { |c| @format.gsub! c, "\\#{c}" }
+@format.gsub! '?', '.'
+@format.gsub! '~', '.*'
+
+puts @text.match?(Regexp.new @format) ? 'MATCH' : 'FAIL'
+
