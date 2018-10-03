4a1415bc06a934d7e45a3d5367b48bba201b387f
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-03 20:44:40 +0900

    GHOST LEGS を追加

---
+++ practice/community_puzzle/ghost-legs.rb
@@ -0,0 +1,31 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@w, @h = gets.split(" ").collect {|x| x.to_i}
+map = []
+@h.times do
+  line = gets.chomp.squeeze.chars
+  map << [' ', *line]
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+top = map.shift
+bottom = map.pop
+
+map.reverse.each do |line|
+  line.each_with_index do |char, index|
+    if char == '-'
+      bottom[index.pred..index.next] = bottom[index.pred..index.next].reverse
+    end
+  end
+end
+
+top.delete(' ')
+bottom.delete(' ')
+
+puts top.zip(bottom).map(&:join)
+
