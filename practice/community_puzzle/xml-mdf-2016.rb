6f9519e5240aab80ccce3ae379034b81153c4f1b
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-11 23:39:56 +0900

    XML MDF-2016 を追加

---
+++ practice/community_puzzle/xml-mdf-2016.rb
@@ -0,0 +1,29 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@sequence = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+depths = {}
+depth  = 0
+until @sequence.empty?
+  @sequence.delete_prefix!(char = @sequence.chr)
+  if char == '-'
+    @sequence.delete_prefix!(char = @sequence.chr)
+    (depths[char] ||= []).push depth
+    depth -= 1
+  else
+    depth += 1
+  end
+end
+
+depths.transform_values! do |value|
+  value.map { |v| Rational(1, v) }.sum
+end
+
+puts depths.max_by(&:last)[0]
+
