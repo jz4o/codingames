3d7eec7b28140e619ce199221a9f3792c3f5a0b7
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-12 23:06:22 +0900

    REVERSED LOOK-AND-SAY を追加

---
+++ practice/community_puzzle/reversed-look-and-say.rb
@@ -0,0 +1,45 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@s = gets.chomp
+
+class String
+  def encodable?
+    encode && self != encode
+  end
+
+  def encode
+    return unless self.size.even?
+
+    result = ''
+    self.chars.each_slice(2) do |count, value|
+      result += value * count.to_i
+    end
+    decodable?(result) ? result : nil
+  end
+
+  def encode!
+    self.replace self.encode
+  end
+
+  private
+
+  def decodable?(encoded_str)
+    result = ''
+    until encoded_str.empty?
+      char = encoded_str.chr
+      group = encoded_str.match(/\A#{char}+/).to_s
+      encoded_str = encoded_str.delete_prefix group
+      result += [group.size, char].join
+    end
+    result == self
+  end
+end
+
+loop do
+  break unless @s.encodable?
+  @s.encode!
+end
+
+puts @s
+
