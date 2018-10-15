4490690f232c096b90db00a077269872de5baa12
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-15 21:52:35 +0900

    BRUCE LEE を追加

---
+++ practice/community_puzzle/bruce-lee.rb
@@ -0,0 +1,25 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@encrypt = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "INVALID"
+
+invalid_flag = false
+binaries = ''
+@encrypt.split(' ').each_slice(2) do |binary, count|
+  unless ['0', '00'].include?(binary) && count
+    invalid_flag = true and break
+  end
+  binaries += (binary == '0' ? '1' : '0') * count.size
+end
+
+if invalid_flag || (binaries.size % 7) != 0
+  puts :INVALID
+else
+  puts binaries.scan(/.{7}/).map { |binary| binary.to_i(2).chr }.join
+end
+
