ad69dcddd0c206d980c70f71e178724f15d8458f
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-18 22:05:27 +0900

    RUBIK® を追加

---
+++ practice/community_puzzle/rubi.rb
@@ -0,0 +1,14 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+cube_count = @n ** 3
+invisible_cube_count = [@n - 2, 0].max ** 3
+puts cube_count - invisible_cube_count
+
