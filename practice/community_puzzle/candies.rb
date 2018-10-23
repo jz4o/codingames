748d20e5f5a0eee5fe1a50261cdace91c305f7cd
jz4o <26317360+jz4o@users.noreply.github.com>
2018-10-23 22:25:43 +0900

    CANDIES を追加

---
+++ practice/community_puzzle/candies.rb
@@ -0,0 +1,27 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n, @k = gets.split(" ").collect {|x| x.to_i}
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+# puts "answer"
+
+results = []
+temps = (1..@k).map { |k| [k] }
+while temps.any?
+  temp = temps.pop
+  if temp.sum == @n
+    results << temp
+  elsif temp.sum < @n
+    (1..@k).each do |k|
+      temps << temp + [k]
+    end
+  end
+end
+
+results.reverse_each do |temp|
+  puts temp.join(' ')
+end
+
