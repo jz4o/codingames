a3d7a3e3f6920166b680ef29bd31d87847354f73
jz4o <26317360+jz4o@users.noreply.github.com>
2018-12-01 17:40:42 +0900

    各言語未実装ファイル一覧表示スクリプトを修正
    ・言語名ディレクトリ以下のパスも出力するよう修正

--- vendor/unimplemented_list.rb
+++ vendor/unimplemented_list.rb
@@ -1,11 +1,11 @@
-# 指定された言語の解答済みファイル名一覧を返す
+# 指定された言語の解答済みファイル一覧を返す
 #
 # @param [Array] dir 言語名(複数指定可能)
 #
-# @return [Array] 解答済みファイル名一覧
+# @return [Array] 解答済みファイル一覧
 def get_file_names(*dir_name)
   file_paths = Dir.glob("#{ROOT_DIR}/{#{dir_name.join(',')}}/**/*.*")
-  file_paths.map { |file_path| File.basename file_path, '.*' }.uniq.sort
+  file_paths.map { |file_path| file_path.match("#{ROOT_DIR}/[^/]+/([^.]+)")[1] }.uniq.sort
 end
 
 # codingames ディレクトリ
@@ -15,18 +15,18 @@ ROOT_DIR = File.expand_path '../', __dir__
 # ※ 初めに Onboarding を解答する想定
 LANGUAGES = Dir.glob('**/onboarding.*', base: ROOT_DIR).map { |path| path.sub /\/.*/, '' }.sort
 
-# 解答済みのファイル名一覧
+# 解答済みのファイル一覧
 FILE_NAMES = get_file_names LANGUAGES
 
 # 出力用配列
 output_list = []
 
-# 一部言語でのみ解答しているファイル名を各言語ごとに出力
+# 一部言語でのみ解答しているファイルを各言語ごとに出力
 LANGUAGES.each do |language|
-  # 該当言語で解答済みのファイル名一覧
+  # 該当言語で解答済みのファイル一覧
   implemented_file_names = get_file_names language
 
-  # 該当言語で未解答のファイル名一覧
+  # 該当言語で未解答のファイル一覧
   unimplemented_file_names = FILE_NAMES - implemented_file_names
 
   # 該当言語で未解答のファイルが存在する場合
