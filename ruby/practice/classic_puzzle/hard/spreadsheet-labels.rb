c2ebed0d33672f57c644eb2a05b2a5873e1f8935
jz4o <26317360+jz4o@users.noreply.github.com>
2019-07-01 23:17:31 +0900

    ruby/Spreadsheet Labels を追加

---
+++ ruby/practice/classic_puzzle/hard/spreadsheet-labels.rb
@@ -0,0 +1,45 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@n = gets.to_i
+inputs = gets.split(" ")
+labels = []
+for i in 0..(@n-1)
+    label = inputs[i]
+    labels.push label
+end
+
+class String
+    ALPHAS = ('A'..'Z').to_a.freeze
+
+    def to_alpha_label
+        result = ''
+        int = self.to_i
+        while int > 0 do
+            i = int % ALPHAS.size
+            if i == 0
+                i = ALPHAS.size
+                int -= 1
+            end
+
+            int = int > ALPHAS.size ? int / ALPHAS.size : 0
+            result = ALPHAS[i - 1] + result
+        end
+        result
+    end
+
+    def to_int_label
+        result = 0
+        self.chars.reverse.each_with_index do |i, index|
+            result += (ALPHAS.size ** index) * (ALPHAS.index(i) + 1)
+        end
+        result
+    end
+end
+
+results = []
+labels.each do |label|
+    results.push label.to_i.zero? ? label.to_int_label : label.to_alpha_label
+end
+
+puts results.join(' ')
