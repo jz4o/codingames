d5e9e37ab8aea3a21fa2a6e4373061bb993793b5
jz4o <26317360+jz4o@users.noreply.github.com>
2020-01-21 00:53:33 +0900

    add typescript/Add'em Up

---
+++ typescript/practice/classic_puzzle/easy/addem-up.ts
@@ -0,0 +1,27 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+let xs: number[] = [];
+for (let i = 0; i < N; i++) {
+    const x: number = parseInt(inputs[i]);
+    xs.push(x);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+let totalCost: number = 0
+while (xs.length >= 2) {
+    xs.sort(function(a: number, b: number) { return a < b ? -1 : 1 });
+
+    const cost: number = xs.shift() + xs.shift();
+    totalCost += cost;
+    xs.push(cost);
+}
+
+// console.log('0');
+console.log(totalCost);
