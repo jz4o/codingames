368a84a4a8b577d93ad72a8c3433ded19b781594
jz4o <26317360+jz4o@users.noreply.github.com>
2020-01-25 23:29:57 +0900

    add typescript/Carmichael numbers

---
+++ typescript/practice/classic_puzzle/medium/carmichael-numbers.ts
@@ -0,0 +1,59 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n: number = parseInt(readline());
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+const primeFlag: boolean = isPrime(n);
+let carmichaelFlag: boolean = !primeFlag;
+
+for (const primeFactor of primeDivision(n)) {
+    if ((n - 1) % (primeFactor - 1) !== 0) {
+        carmichaelFlag = false;
+        break;
+    }
+}
+
+// console.log('YES|NO');
+console.log(carmichaelFlag ? 'YES' : 'NO');
+
+
+function isPrime(num: number) {
+    if (num % 2 === 0) {
+        return false;
+    }
+
+    for (let i = 3; i < num; i+=2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function primeDivision(num) {
+    let divisions: number[] = []
+
+    while (num % 2 === 0) {
+        divisions.push(2);
+        num /= 2;
+    }
+
+    let i: number = 3;
+    while (num !== 1) {
+        if (num % i === 0) {
+            divisions.push(i);
+            num /= i;
+        } else {
+            i += 2;
+        }
+    }
+
+    return divisions;
+}
+
