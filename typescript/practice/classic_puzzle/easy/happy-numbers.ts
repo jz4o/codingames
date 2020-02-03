2817de267b3befac6654a1976472086d260f8404
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-03 22:14:16 +0900

    add typescript/Happy Numbers

---
+++ typescript/practice/classic_puzzle/easy/happy-numbers.ts
@@ -0,0 +1,38 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+let xs: string[] = []
+for (let i = 0; i < N; i++) {
+    const x: string = readline();
+
+    xs.push(x);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+const HAPPY_NUMBER: string = '1';
+const HAPPY_FACE: string = ':)';
+const UNHAPPY_FACE: string = ':(';
+
+let results: string[] = []
+for (const x of xs) {
+    let temp: string = x;
+
+    let exitNumbers: string[] = [HAPPY_NUMBER];
+    while (!exitNumbers.includes(temp)) {
+        exitNumbers.push(temp);
+        temp = temp.split('').map(d => parseInt(d) ** 2).reduce((sum, d) => sum + d, 0).toString();
+    }
+
+    const resultFace = temp === HAPPY_NUMBER ? HAPPY_FACE : UNHAPPY_FACE;
+    results.push(`${x} ${resultFace}`);
+}
+
+// console.log('23 :)');
+for (const result of results) {
+    console.log(result);
+}
