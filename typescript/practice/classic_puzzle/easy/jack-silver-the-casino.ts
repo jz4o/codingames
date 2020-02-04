f80b22937afd47a4771de8987ddef06d6164bcd3
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-04 23:46:38 +0900

    add typescript/Jack Silver: The Casino

---
+++ typescript/practice/classic_puzzle/easy/jack-silver-the-casino.ts
@@ -0,0 +1,42 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const ROUNDS: number = parseInt(readline());
+const CASH: number = parseInt(readline());
+let result: number = CASH
+for (let i = 0; i < ROUNDS; i++) {
+    const bet = Math.ceil(result / 4);
+    result -= bet;
+
+    const PLAY: string = readline();
+    const playElements: any[] = PLAY.split(' ');
+    const ball: number = playElements[0];
+    const call: string = playElements[1];
+    const target: number = playElements[2];
+
+    switch (call) {
+        case 'PLAIN':
+            if (ball === target) {
+                result += bet * 36;
+            }
+            break;
+        case 'ODD':
+            if (ball % 2 === 1) {
+                result += bet * 2;
+            }
+            break;
+        case 'EVEN':
+            if (ball % 2 === 0 && ball > 0) {
+                result += bet * 2;
+            }
+            break;
+    }
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+// console.log('cash of the target after playing');
+console.log(result);
