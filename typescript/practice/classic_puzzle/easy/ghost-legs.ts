c237e7d27fc3f3fb6ecae8c1059979ba0a959f1f
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-11 23:27:54 +0900

    add typescript/Ghost Legs

---
+++ typescript/practice/classic_puzzle/easy/ghost-legs.ts
@@ -0,0 +1,41 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const W: number = parseInt(inputs[0]);
+const H: number = parseInt(inputs[1]);
+const grid: string[][] = [];
+for (let i = 0; i < H; i++) {
+    const line: string = readline();
+
+    grid.push([' ', ...line.split('').filter((char, index, array) => char !== array[index + 1])]);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+const topLine: string[] = grid.shift();
+const bottomLine: string[] = grid.pop();
+
+for (const line of grid.reverse()) {
+    line.forEach((char, index) => {
+        if (char === '-') {
+            [bottomLine[index - 1], bottomLine[index + 1]] = [bottomLine[index + 1], bottomLine[index - 1]]
+        }
+    });
+}
+
+const results:string[] = topLine.reduce((result, char, index) => {
+    if (char !== ' ') {
+        result.push(`${char}${bottomLine[index]}`);
+    }
+
+    return result;
+}, []);
+
+// console.log('answer');
+for (const result of results) {
+    console.log(result);
+}
