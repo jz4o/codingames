949c846a8e70799382d7becb0cde1a4aa7189a5f
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-12 23:52:33 +0900

    add typescript/Minimal number of swaps

---
+++ typescript/practice/classic_puzzle/medium/minimal-number-of-swaps.ts
@@ -0,0 +1,36 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+const xs: number[] = [];
+for (let i = 0; i < n; i++) {
+    const x: number = parseInt(inputs[i]);
+
+    xs.push(x);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+let lIndex: number = -1;
+let rIndex: number = xs.length;
+let turn: number = 0;
+while (lIndex < rIndex) {
+    lIndex++;
+    while(lIndex < xs.length && xs[lIndex] !== 0) {
+        lIndex++;
+    }
+
+    rIndex--;
+    while(rIndex >= 0 && xs[rIndex] !== 1) {
+        rIndex--;
+    }
+
+    turn++;
+}
+
+// console.log('answer');
+console.log(turn - 1);
