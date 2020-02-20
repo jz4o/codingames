868bc2d699b78884db331ca41c5ba9eb6fd5e737
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-20 22:40:04 +0900

    add typescript/XML MDF-2016

---
+++ typescript/practice/classic_puzzle/easy/xml-mdf-2016.ts
@@ -0,0 +1,40 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const sequence: string = readline();
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+const depths: {[key: string]: number[]} = {};
+let depth: number = 0;
+const sequenceChars: string[] = sequence.split('');
+while (sequenceChars.length > 0) {
+    let char: string = sequenceChars.shift();
+
+    if (char === '-') {
+        char = sequenceChars.shift();
+
+        depths[char] = depths[char] || [];
+        depths[char].push(depth);
+
+        depth--;
+    } else {
+        depth++;
+    }
+}
+
+let maximumValueKey: string;
+let maximumValue: number = 0;
+Object.keys(depths).forEach(key => {
+    const sum: number = depths[key].reduce((sum, keyDepth) => sum += 1 / keyDepth, 0);
+    if (maximumValue < sum) {
+        maximumValueKey = key;
+        maximumValue = sum;
+    }
+});
+
+// console.log('answer');
+console.log(maximumValueKey);
