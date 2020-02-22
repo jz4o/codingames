fff9083849466b3efb33c8d9d31a3004da9e1640
jz4o <26317360+jz4o@users.noreply.github.com>
2020-02-22 23:19:33 +0900

    add typescript/Candies

---
+++ typescript/practice/classic_puzzle/hard/candies.ts
@@ -0,0 +1,35 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const n: number = parseInt(inputs[0]);
+const k: number = parseInt(inputs[1]);
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+let results: number[][] = []
+let temps: number[][] = []
+for (let i = 1; i <= k; i++) {
+    temps.push([i]);
+}
+
+while (temps.length > 0) {
+    const temp: number[] = temps.pop();
+    const tempSum: number = temp.reduce((sum, t) => sum + t, 0);
+
+    if (tempSum === n) {
+        results.unshift(temp);
+    } else if (tempSum < n) {
+        for (let i = 1; i <= k; i++) {
+            temps.push(temp.concat(i));
+        }
+    }
+}
+
+// console.log('answer');
+for (const result of results) {
+    console.log(result.join(' '));
+}
