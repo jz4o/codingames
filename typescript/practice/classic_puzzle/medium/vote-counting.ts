1fa7a106605f621afe7bd10accc842b0c617f4ba
jz4o <26317360+jz4o@users.noreply.github.com>
2020-03-11 21:34:18 +0900

    add typescript/Vote counting

---
+++ typescript/practice/classic_puzzle/medium/vote-counting.ts
@@ -0,0 +1,44 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+const M: number = parseInt(readline());
+const voters: {[key: string]: number} = {};
+for (let i = 0; i < N; i++) {
+    var inputs: string[] = readline().split(' ');
+    const personName: string = inputs[0];
+    const nbVote: number = parseInt(inputs[1]);
+
+    voters[personName] = nbVote;
+}
+const votes: {[key: string]: string[]} = {}
+for (let i = 0; i < M; i++) {
+    var inputs: string[] = readline().split(' ');
+    const voterName: string = inputs[0];
+    const voteValue: string = inputs[1];
+
+    if (votes[voterName] === undefined) {
+        votes[voterName] = [];
+    }
+
+    votes[voterName].push(voteValue);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+let yes: number = 0;
+let no: number = 0;
+Object.keys(voters).forEach(voterName => {
+    if (votes[voterName] === undefined || votes[voterName].length > voters[voterName]) {
+        return;
+    }
+
+    yes += votes[voterName].filter(vote => vote === 'Yes').length;
+    no += votes[voterName].filter(vote => vote === 'No').length;
+});
+
+// console.log('output');
+console.log(`${yes} ${no}`);
