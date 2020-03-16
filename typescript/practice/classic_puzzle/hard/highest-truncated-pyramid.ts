b268a97b905150523135661332e4381110c583e4
jz4o <26317360+jz4o@users.noreply.github.com>
2020-03-16 23:34:47 +0900

    add typescript/Highest truncated pyramid

---
+++ typescript/practice/classic_puzzle/hard/highest-truncated-pyramid.ts
@@ -0,0 +1,26 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+
+let partCount: number = 1;
+let topStepCount: number = 1;
+let bottomStepCount: number = 1;
+
+while (true) {
+    if (partCount < N) {
+        bottomStepCount++;
+        partCount += bottomStepCount;
+    } else if (N < partCount) {
+        partCount -= topStepCount;
+        topStepCount++;
+    } else {
+        break;
+    }
+}
+
+for (let i = topStepCount; i <= bottomStepCount; i++) {
+    console.log('*'.repeat(i));
+}
