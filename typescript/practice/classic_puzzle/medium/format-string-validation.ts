3344b4b6f2e635611fdae47f429152817a2104df
jz4o <26317360+jz4o@users.noreply.github.com>
2020-03-17 23:44:02 +0900

    add typescript/Format string validation

---
+++ typescript/practice/classic_puzzle/medium/format-string-validation.ts
@@ -0,0 +1,20 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const text: string = readline();
+const format: string = readline();
+
+let regexpFormat: string = format.replace(/\\/g, '\\\\')
+                                 .replace(/\^/g, '\\^')
+                                 .replace(/\*/g, '\\*')
+                                 .replace(/\(/g, '\\(')
+                                 .replace(/\)/g, '\\)')
+                                 .replace(/\[/g, '\\[')
+                                 .replace(/\]/g, '\\]')
+                                 .replace(/\$/g, '\\$')
+                                 .replace(/\?/g, '.')
+                                 .replace(/~/g, '.*');
+
+console.log(text.match(new RegExp(regexpFormat)) ? 'MATCH' : 'FAIL');
