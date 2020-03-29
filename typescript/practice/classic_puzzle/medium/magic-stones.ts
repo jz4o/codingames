33777e6229e97e2a2038db4a4359cf4fd457e49a
jz4o <26317360+jz4o@users.noreply.github.com>
2020-03-29 23:39:37 +0900

    add typescript/Magic stones

---
+++ typescript/practice/classic_puzzle/medium/magic-stones.ts
@@ -0,0 +1,44 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+let stones: number[] = [];
+for (let i = 0; i < N; i++) {
+    const level: number = parseInt(inputs[i]);
+
+    stones.push(level);
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+let stoneCount: number = 0;
+let levels: number[] = stones.reduce((levels, stone) => {
+    if (!levels.includes(stone)) {
+        levels.push(stone);
+    }
+
+    return levels;
+}, []).sort();
+
+while (levels.length > 0) {
+    const level: number = levels.shift();
+    const targetLevelStones: number[] = stones.filter(stone => stone === level);
+    const nextLevelStoneCount: number = Math.trunc(targetLevelStones.length / 2);
+    const targetStoneCount: number = targetLevelStones.length % 2;
+    stones = stones.filter(stone => stone !== level);
+
+    if (nextLevelStoneCount > 0) {
+        const nextLevel: number = level + 1;
+        stones.push(...Array(nextLevelStoneCount).fill(nextLevel));
+        levels.unshift(nextLevel);
+    }
+
+    stoneCount += targetStoneCount;
+}
+
+// console.log('1');
+console.log(stoneCount);
