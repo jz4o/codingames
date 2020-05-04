6a8e2c05532fa151fb720b3b45e640f5c30e962d
jz4o <26317360+jz4o@users.noreply.github.com>
2020-05-04 22:52:57 +0900

    add typescript/Gravity

---
+++ typescript/practice/classic_puzzle/medium/gravity.ts
@@ -0,0 +1,28 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const width: number = parseInt(inputs[0]);
+const height: number = parseInt(inputs[1]);
+const grid: string[][] = [];
+for (let i = 0; i < height; i++) {
+    const line: string = readline();
+
+    grid.push(line.split(''));
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const transpose = array => array[0].map((_value, index) => array.map(row => row[index]));
+
+const transposedGrid: string[][] = transpose(grid);
+const transposedMovedGrid: string[][] = transposedGrid.map(line => line.sort().reverse());
+const movedGrid: string[][] = transpose(transposedMovedGrid);
+
+// console.log('answer');
+movedGrid.forEach(line => {
+    console.log(line.join(''));
+});
