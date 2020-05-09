c9f1faa722fa8623b28452c412b9a3700df8844e
jz4o <26317360+jz4o@users.noreply.github.com>
2020-05-09 22:10:19 +0900

    add typescript/Gravity Tumbler

---
+++ typescript/practice/classic_puzzle/medium/gravity-tumbler.ts
@@ -0,0 +1,30 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const width: number = parseInt(inputs[0]);
+const height: number = parseInt(inputs[1]);
+const count: number = parseInt(readline());
+const rasters: string[][] = [];
+for (let i = 0; i < height; i++) {
+    const raster: string = readline();
+
+    rasters.push(raster.split(''));
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const transpose = array => array[0].map((_value, index) => array.map(row => row[index]));
+
+const result: string[][] = Array(count).fill(null).reduce((result, _) => {
+    return transpose(result.map(line => line.sort())).reverse();
+}, rasters);
+
+// console.log('...');
+// console.log('write ###');
+result.forEach(line => {
+    console.log(line.join(''));
+});
