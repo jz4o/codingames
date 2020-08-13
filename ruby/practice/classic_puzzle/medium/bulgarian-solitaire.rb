be02927f055f64f12ed8b3ec125f644c8dc540ab
jz4o <26317360+jz4o@users.noreply.github.com>
2020-08-13 17:49:58 +0900

    add ruby/Bulgarian solitaire

---
+++ ruby/practice/classic_puzzle/medium/bulgarian-solitaire.rb
@@ -0,0 +1,36 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = gets.to_i
+inputs = gets.split(' ')
+piles = []
+(0...n).each do |i|
+  c = inputs[i].to_i
+
+  piles << c
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+piles.reject!(&:zero?)
+
+same_configuration_index = 0
+configurations = []
+
+loop do
+  configurations << piles.sort.join(' ')
+
+  piles_size = piles.size
+  piles = piles.map(&:pred).reject(&:zero?) << piles_size
+
+  configuration = piles.sort.join(' ')
+
+  if configurations.include? configuration
+    same_configuration_index = configurations.index configuration
+    break
+  end
+end
+
+# puts "1"
+puts configurations.size - same_configuration_index
