6b7583a84e0e68505a3579a54b1737d96d08ce91
jz4o <26317360+jz4o@users.noreply.github.com>
2020-08-17 23:31:45 +0900

    add typescript/Bulgarian solitaire

---
+++ typescript/practice/classic_puzzle/medium/bulgarian-solitaire.ts
@@ -0,0 +1,38 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N: number = parseInt(readline());
+var inputs: string[] = readline().split(' ');
+let piles: number[] = [];
+for (let i = 0; i < N; i++) {
+    const C: number = parseInt(inputs[i]);
+
+    piles.push(C);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+piles = piles.filter(pile => pile !== 0);
+
+let sameConfigurationIndex: number = 0;
+const configurations: string[] = [];
+
+while (true) {
+    configurations.push(piles.sort().join(' '));
+
+    const pilesSize: number = piles.length;
+    piles = piles.map(pile => pile - 1).filter(pile => pile !== 0).concat(pilesSize);
+
+    const configuration: string = piles.sort().join(' ');
+
+    if (configurations.includes(configuration)) {
+        sameConfigurationIndex = configurations.indexOf(configuration);
+        break;
+    }
+}
+
+// console.log('1');
+console.log(configurations.length - sameConfigurationIndex);
