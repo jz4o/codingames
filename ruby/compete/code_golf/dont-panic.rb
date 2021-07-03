20db8f71c74b6371b31aa4fc252363c3dac055f6
jz4o <26317360+jz4o@users.noreply.github.com>
2021-07-03 23:45:30 +0900

    refactor ruby/Don't Panic

--- ruby/compete/code_golf/dont-panic.rb
+++ ruby/compete/code_golf/dont-panic.rb
@@ -5,4 +5,4 @@ a=t.map(&s)
 i=[]
 j=[]
 a[7].times{k=t.map(&s);i<<k[0];j<<k[1]}
-loop{l,m,n=t;m=m.to_i;o=i.index(l.to_i);q=o ?j[o]:a[4];puts (q<m&&n[0]=='L')||(m<q&&n[0]=='R')||q==m ?'WAIT':'BLOCK'}
+loop{l,m,n=t;m=m.to_i;o=i.index(l.to_i);q=o ?j[o]:a[4];puts (q<m&&n[0]=='L')||(m<q&&n[0]=='R')||q==m ?:WAIT: :BLOCK}
