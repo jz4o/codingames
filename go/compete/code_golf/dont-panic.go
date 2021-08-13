52598afc332a001ca0c57eaf1bb6382daa3b8095
jz4o <26317360+jz4o@users.noreply.github.com>
2021-08-13 23:51:12 +0900

    refactor go/Don't Panic

--- go/compete/code_golf/dont-panic.go
+++ go/compete/code_golf/dont-panic.go
@@ -1,20 +1,18 @@
 package main
 import ."fmt"
 func main(){
-var a,b,c,d,e,f,g,h int
+var a,b,c,d,e,f,g,h,l,m,n,o int
 Scan(&a,&b,&c,&d,&e,&f,&g,&h)
 j:=[]int{}
 k:=[]int{}
-for i:=0;i<h;i++{var l,m int
-Scan(&l,&m)
+for i:=0;i<h;i++{Scan(&l,&m)
 j=append(j,l)
 k=append(k,m)}
-for{var n,o int
-var p string
+for{var p string
 Scan(&n,&o,&p)
 q:=e
 for i:=0;i<len(j);i++{if j[i]==n{q=k[i]
 break}}
 t:="BLOCK"
-if(q<o&&p=="LEFT")||(o<q&&p=="RIGHT")||q==o{t="WAIT"}
+if(q<o&&p=="LEFT")||(o<q&&p[0]=='R')||q==o{t="WAIT"}
 Println(t)}}
