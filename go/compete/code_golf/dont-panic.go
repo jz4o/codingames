87b4eb8ca14ee3ce00b0548a4081f6ab9eea4344
jz4o <26317360+jz4o@users.noreply.github.com>
2021-08-14 23:01:38 +0900

    refactor go/Don't Panic

--- go/compete/code_golf/dont-panic.go
+++ go/compete/code_golf/dont-panic.go
@@ -1,18 +1,18 @@
 package main
 import ."fmt"
 func main(){
-var a,b,c,d,e,f,g,h,l,m,n,o int
-Scan(&a,&b,&c,&d,&e,&f,&g,&h)
-j:=[]int{}
-k:=[]int{}
-for i:=0;i<h;i++{Scan(&l,&m)
-j=append(j,l)
-k=append(k,m)}
-for{var p string
-Scan(&n,&o,&p)
-q:=e
-for i:=0;i<len(j);i++{if j[i]==n{q=k[i]
+var a,b,c,d,e,f,z int
+Scan(&z,&z,&z,&z,&a,&z,&z,&b)
+var g string
+h:=[]int{}
+i:=[]int{}
+for z=0;z<b;z++{Scan(&c,&d)
+h=append(h,c)
+i=append(i,d)}
+for{Scan(&e,&f,&g)
+q:=a
+for z=0;z<len(h);z++{if h[z]==e{q=i[z]
 break}}
 t:="BLOCK"
-if(q<o&&p=="LEFT")||(o<q&&p[0]=='R')||q==o{t="WAIT"}
+if(q<f&&g=="LEFT")||(f<q&&g[0]=='R')||q==f{t="WAIT"}
 Println(t)}}
