e91ed7b9a2bf2e2e850d644f5f1d26ab8c3fd8e7
jz4o <26317360+jz4o@users.noreply.github.com>
2021-08-15 23:26:56 +0900

    refactor ruby/Don't Panic

--- ruby/compete/code_golf/dont-panic.rb
+++ ruby/compete/code_golf/dont-panic.rb
@@ -1,8 +1,8 @@
 STDOUT.sync=1
 s=:to_i
 def t;gets.split;end
-a=t.map(&s)
+a=t.map &s
 i=[]
 j=[]
-a[7].times{k=t.map(&s);i<<k[0];j<<k[1]}
+a[7].times{k=t.map &s;i<<k[0];j<<k[1]}
 loop{l,m,n=t;m=m.to_i;o=i.index(l.to_i);q=o ?j[o]:a[4];puts (q<m&&n[0]=='L')||(m<q&&n[0]=='R')||q==m ?:WAIT: :BLOCK}
