e477005009584ea13db6ad54cbe07609acf77d54
jz4o <26317360+jz4o@users.noreply.github.com>
2021-08-21 23:23:42 +0900

    add typescript/Decode the message

---
+++ typescript/practice/classic_puzzle/easy/decode-the-message.ts
@@ -0,0 +1,30 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const P: number = parseInt(readline());
+const C: string = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const decode = (encoded_value: number, alphabet: string): string => {
+    let result: string = '';
+    let int: number = encoded_value + 1;
+    while (int > 0) {
+        let mod = int % alphabet.length;
+        int = Math.floor(int / alphabet.length);
+        if (mod === 0) {
+            mod = alphabet.length;
+            int--;
+        }
+
+        result += alphabet[mod - 1];
+    }
+
+    return result;
+};
+
+// console.log('Good Luck :->');
+console.log(decode(P, C));
