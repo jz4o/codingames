ae4560c1a67723ec11c42faa6c16083685c6d472
jz4o <26317360+jz4o@users.noreply.github.com>
2021-09-25 21:52:18 +0900

    add python3/The Gift

---
+++ python3/practice/classic_puzzle/medium/the-gift.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+c = int(input())
+budgets = []
+for i in range(n):
+    b = int(input())
+
+    budgets.append(b)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+results = []
+if sum(budgets) < c:
+    results.append('IMPOSSIBLE')
+else:
+    lack = c
+    budgets.sort()
+    for index, budget in enumerate(budgets):
+        average = math.floor(lack / (len(budgets) - index))
+        pay = min(budget, average)
+
+        lack -= pay
+
+        results.append(pay)
+
+# print("IMPOSSIBLE")
+for result in results:
+    print(result)
