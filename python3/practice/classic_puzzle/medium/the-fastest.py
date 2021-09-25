0af5ee2902297b0d2c3b50f96f6d8b7ff901e36c
jz4o <26317360+jz4o@users.noreply.github.com>
2021-09-25 21:38:20 +0900

    add python3/The Fastest

---
+++ python3/practice/classic_puzzle/medium/the-fastest.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+ts = []
+for i in range(n):
+    t = input()
+
+    ts.append(t)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+result = sorted(ts)[0]
+
+# print("answer")
+print(result)
