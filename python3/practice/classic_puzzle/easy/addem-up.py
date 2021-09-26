7ef963a5943b83912ecb5bde8727355f3944ba42
jz4o <26317360+jz4o@users.noreply.github.com>
2021-09-26 15:22:40 +0900

    add python3/Add'em Up

---
+++ python3/practice/classic_puzzle/easy/addem-up.py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+integers = []
+for i in input().split():
+    x = int(i)
+
+    integers.append(x)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+total_cost = 0
+while len(integers) >= 2:
+    integers.sort()
+
+    first_integer = integers.pop(0)
+    second_integer = integers.pop(0)
+
+    cost = first_integer + second_integer
+
+    total_cost += cost
+
+    integers.append(cost)
+
+# print("0")
+print(total_cost)
