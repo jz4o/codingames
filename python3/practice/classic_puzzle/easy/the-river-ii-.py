9496d3ee098c464ace38bed196994b1f519569fb
jz4o <26317360+jz4o@users.noreply.github.com>
2021-09-28 23:12:20 +0900

    add python3/The River II.

---
+++ python3/practice/classic_puzzle/easy/the-river-ii-.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+r_1 = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+result = 'NO'
+for i in reversed(range(1, r_1 - 1)):
+    if i + sum(map(int, str(i))) == r_1:
+        result = 'YES'
+        break
+
+# print("YES|NO")
+print(result)
