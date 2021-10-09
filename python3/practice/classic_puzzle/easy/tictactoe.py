c67a8ffe7e17f5ca70cabfd9d0d30ea07a3705da
jz4o <26317360+jz4o@users.noreply.github.com>
2021-10-09 22:37:19 +0900

    add python3/TicTacToe

---
+++ python3/practice/classic_puzzle/easy/tictactoe.py
@@ -0,0 +1,43 @@
+import sys
+import math
+
+import numpy as np
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+grid = np.empty((0, 3), int)
+for i in range(3):
+    line = input()
+
+    grid = np.append(grid, np.array([list(line)]), axis = 0)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+origin_grid = grid.copy()
+
+# check row
+for index in range(3):
+    if sorted(grid[index,:]) == list('.OO'):
+        grid[index,:] = list('OOO')
+
+# check column
+if (origin_grid == grid).all():
+    for index in range(3):
+        if sorted(grid[:,index]) == list('.OO'):
+            grid[:,index] = list('OOO')
+
+# check diagonal
+if (origin_grid == grid).all() and sorted(map(lambda index: grid[index, index], range(3))) == list('.OO'):
+    for index in range(3):
+        grid[index, index] = 'O'
+if (origin_grid == grid).all() and sorted(map(lambda index: grid[index, 2 - index], range(3))) == list('.OO'):
+    for index in range(3):
+        grid[index, 2 - index] = 'O'
+
+results = ['false'] if (origin_grid == grid).all() else map(lambda row: ''.join(row), grid)
+
+# print("answer")
+for result in results:
+    print(result)
