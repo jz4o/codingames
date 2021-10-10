0cfbc6d93e30f30c5e0bd16995426d4e77d16c4a
jz4o <26317360+jz4o@users.noreply.github.com>
2021-10-10 23:16:58 +0900

    add python3/Sum of Spiral's Diagonals

---
+++ python3/practice/classic_puzzle/easy/sum-of-spirals-diagonals.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+result = 0
+for side_length in range(n, 0, -2):
+    base = n**2 - side_length**2 + 1
+    result += base if side_length <= 1 else sum(map(lambda i: i * (side_length - 1) + base, range(0, 4)))
+
+print(result)
