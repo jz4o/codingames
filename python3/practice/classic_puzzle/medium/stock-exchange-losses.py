9c654210a25c2fa55e1040590c98ff082ef06962
jz4o <26317360+jz4o@users.noreply.github.com>
2021-10-21 23:13:24 +0900

    add python3/Stock Exchange Losses

---
+++ python3/practice/classic_puzzle/medium/stock-exchange-losses.py
@@ -0,0 +1,33 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+values = []
+for i in input().split():
+    v = int(i)
+
+    values.append(v)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+max_value = 0
+max_diff = 0
+for index, value in enumerate(values):
+    if index >= len(values) - 1:
+        break
+
+    if max_value >= value:
+        continue
+
+    max_value = value
+
+    diff = min(values[index + 1:]) - value
+    if max_diff >= diff:
+        max_diff = diff
+
+# print("answer")
+print(max_diff)
