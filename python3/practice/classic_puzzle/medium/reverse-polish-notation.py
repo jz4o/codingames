e936cc0aecd2762df304c515d5efc18b9b2c5fa6
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-05 23:18:24 +0900

    add python3/Reverse Polish Notation

---
+++ python3/practice/classic_puzzle/medium/reverse-polish-notation.py
@@ -0,0 +1,69 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+instructions = []
+for instruction in input().split():
+    # pass
+    instructions.append(instruction)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+stack = []
+try:
+    for instruction in instructions:
+        if instruction == 'ADD':
+            r = int(stack.pop())
+            l = int(stack.pop())
+            stack.append(l + r)
+
+        elif instruction == 'SUB':
+            r = int(stack.pop())
+            l = int(stack.pop())
+            stack.append(l - r)
+
+        elif instruction == 'MUL':
+            r = int(stack.pop())
+            l = int(stack.pop())
+            stack.append(l * r)
+
+        elif instruction == 'DIV':
+            r = int(stack.pop())
+            l = int(stack.pop())
+            stack.append(int(l / r))
+
+        elif instruction == 'MOD':
+            r = int(stack.pop())
+            l = int(stack.pop())
+            stack.append(l % r)
+
+        elif instruction == 'POP':
+            stack.pop()
+
+        elif instruction == 'DUP':
+            last = stack.pop()
+            stack += [last, last]
+
+        elif instruction == 'SWP':
+            r = stack.pop()
+            l = stack.pop()
+            stack += [r, l]
+
+        elif instruction == 'ROL':
+            last = int(stack.pop())
+            stack.append(stack.pop(-last))
+
+        else:
+            stack.append(instruction)
+
+except (ZeroDivisionError, IndexError):
+    stack.append('ERROR')
+
+result = ' '.join(str(i) for i in stack)
+
+# print("answer")
+print(result)
