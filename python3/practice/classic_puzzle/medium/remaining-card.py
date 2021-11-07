3befcf7e6b8ac31d557f52d372ecb4233c08e7f8
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-07 23:48:57 +0900

    add python3/Remaining Card

---
+++ python3/practice/classic_puzzle/medium/remaining-card.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+base = 2
+step = 2
+max_num = n
+while base < max_num:
+    prev_max = max_num
+    max_num = int((max_num - base) / step) * step + base
+
+    if prev_max == max_num:
+        base += step
+
+    step *= 2
+
+# print("answer")
+print(max_num)
