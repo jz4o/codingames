c32f504d6b23c2dc95410d708f046cd4e22262b9
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-08 23:07:15 +0900

    add python3/Number of letters in a number - Binary

---
+++ python3/practice/classic_puzzle/medium/number-of-letters-in-a-number---binary.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+start, n = [int(i) for i in input().split()]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+result = start
+for i in range(n):
+    temp = len(format(result, 'b').replace('0', 'zero').replace('1', 'one'))
+    if result == temp:
+        break
+
+    result = temp
+
+# print("42")
+print(result)
