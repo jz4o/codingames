6b72420488f1e2f52611126827bd6581b09ebca0
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-09 23:16:28 +0900

    add python3/Number of paths between 2 points

---
+++ python3/practice/classic_puzzle/medium/number-of-paths-between-2-points.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+m = int(input())
+n = int(input())
+grid = []
+grid.append([1] * (n + 1))
+for i in range(m):
+    row = input()
+
+    grid.append([1, *map(int, row)])
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+path_map = [[0] * (n + 1) for i in range(m + 1)]
+
+grid[1][1] = 1
+path_map[1][1] = 1
+
+for row_index in range(1, m + 1):
+    for column_index in range(1, n + 1):
+        if grid[row_index][column_index] != 0:
+            continue
+
+        top_math_paths = path_map[row_index - 1][column_index]
+        left_math_paths = path_map[row_index][column_index - 1]
+        path_map[row_index][column_index] = top_math_paths + left_math_paths
+
+# print("answer")
+print(path_map[m][n])
