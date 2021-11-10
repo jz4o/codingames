7bb0f1e6a7af7651e272f6377e41e334f6025021
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-10 23:40:39 +0900

    add python3/Numeral system

---
+++ python3/practice/classic_puzzle/medium/numeral-system.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+equality = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+NUMERALS = list(map(str, range(10))) + [chr(i + ord('A')) for i in range(26)]
+
+x, y, z = re.findall('(.+)\+(.+)=(.+)', equality)[0]
+
+max_numeral = next(filter(lambda n: n in equality, reversed(NUMERALS)))
+minimum_n = NUMERALS.index(max_numeral) + 1
+
+result = next(filter(lambda i: int(x, i) + int(y, i) == int(z, i), range(minimum_n, len(NUMERALS) + 1)))
+
+# print("answer")
+print(result)
