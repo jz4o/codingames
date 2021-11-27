1f73be9ddda4f04ee25adfb58483f9ddb734d841
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-27 22:13:52 +0900

    add python3/Brackets, Extended Edition

---
+++ python3/practice/classic_puzzle/medium/brackets-extended-edition.py
@@ -0,0 +1,35 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+expressions = []
+for i in range(n):
+    expression = input()
+    expressions.append(expression)
+for i in range(n):
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    expression = expressions[i]
+
+    expression = ''.join(re.findall('[()\[\]{}<>]', expression))
+    expression = expression.replace('(', ')')
+    expression = expression.replace('[', ']')
+    expression = expression.replace('{', '}')
+    expression = expression.replace('<', '>')
+
+    brackets = []
+    for c in expression:
+        if len(brackets) > 0 and c == brackets[-1]:
+            brackets.pop()
+        else:
+            brackets.append(c)
+
+    # print("true")
+    print('true' if len(brackets) <= 0 else 'false')
