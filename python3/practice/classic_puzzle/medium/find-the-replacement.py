b08a7e3b0d11a8d7cbf7d50844d280ffeb69fbea
jz4o <26317360+jz4o@users.noreply.github.com>
2021-11-30 22:07:35 +0900

    add python3/Find the replacement

---
+++ python3/practice/classic_puzzle/medium/find-the-replacement.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+x = input()
+y = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+replace_dict = {xi: yi for xi, yi in zip(x, y) if xi != yi}
+
+replace_result = x.translate(str.maketrans(replace_dict))
+
+result = ''
+if x == y:
+    result = 'NONE'
+elif replace_result == y:
+    result = '\n'.join([f'{k}->{v}' for k, v in replace_dict.items()])
+else:
+    result = "CAN'T"
+
+# print("anwser")
+print(result)
