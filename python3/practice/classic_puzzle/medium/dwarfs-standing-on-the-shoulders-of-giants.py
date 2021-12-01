6b7b51907dc30fd4d99320a2bb59da26e3492d37
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-01 23:36:43 +0900

    add python3/Dwarfs standing on the shoulders of giants

---
+++ python3/practice/classic_puzzle/medium/dwarfs-standing-on-the-shoulders-of-giants.py
@@ -0,0 +1,30 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of relationships of influence
+relations = {}
+for i in range(n):
+    # x: a relationship of influence between two people (x influences y)
+    x, y = [int(j) for j in input().split()]
+
+    if x not in relations:
+        relations[x] = []
+
+    relations[x].append(y)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+targets = relations.keys()
+deep_level = 0
+while len(targets) > 0:
+    deep_level += 1
+
+    targets = sum([relations[target] for target in targets if target in relations], [])
+
+# The number of people involved in the longest succession of influences
+# print("2")
+print(deep_level)
