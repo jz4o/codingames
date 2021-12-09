3aa470ba8f865cd8b80d1114d42256ba233358fb
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-09 22:23:33 +0900

    add python3/Bulgarian solitaire

---
+++ python3/practice/classic_puzzle/medium/bulgarian-solitaire.py
@@ -0,0 +1,37 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+piles = []
+for i in input().split():
+    c = int(i)
+
+    piles.append(c)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+piles = list(filter(lambda c: c != 0, piles))
+
+same_configuration_index = 0
+configurations = []
+while True:
+    configurations.append(' '.join(map(str, sorted(piles))))
+
+    piles_size = len(piles)
+    piles = list(filter(lambda c: c != 0, map(lambda c: c - 1, piles)))
+    piles.append(piles_size)
+
+    configuration = ' '.join(map(str, sorted(piles)))
+
+    if configuration in configurations:
+        same_configuration_index = configurations.index(configuration)
+        break
+
+result = len(configurations) - same_configuration_index
+
+# print("1")
+print(result)
