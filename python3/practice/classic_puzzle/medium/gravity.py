b0c75a967d87d2c9a95819ae1591fea592024471
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-10 23:30:33 +0900

    add python3/Gravity

---
+++ python3/practice/classic_puzzle/medium/gravity.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+import pandas as pd
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+width, height = [int(i) for i in input().split()]
+grid = []
+for i in range(height):
+    line = input()
+
+    grid.append(list(line))
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+grid = pd.DataFrame(grid)
+grid = pd.DataFrame(map(lambda row: reversed(sorted(row)), grid.T.values))
+
+results = map(lambda row: ''.join(row), grid.T.values)
+
+# print("answer")
+for result in results:
+    print(result)
