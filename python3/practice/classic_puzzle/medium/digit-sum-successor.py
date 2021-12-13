41858229f2722fd4b1b4e4db599430869de25429
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-13 23:45:27 +0900

    add python3/Digit sum successor

---
+++ python3/practice/classic_puzzle/medium/digit-sum-successor.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+digits = [0, *list(map(int, str(n)))]
+
+decrement_digit = list(filter(lambda d: d != 0, digits))[-1]
+decrement_index = ''.join(map(str, digits)).rindex(str(decrement_digit))
+digits[decrement_index] -= 1
+
+increment_digit = list(filter(lambda d: d != 9, digits[:decrement_index]))[-1]
+increment_index = ''.join(map(str, digits[:decrement_index])).rindex(str(increment_digit))
+digits[increment_index] += 1
+
+digits[increment_index + 1:] = sorted(digits[increment_index + 1:])
+
+result = int(''.join(map(str, digits)))
+
+# print("11")
+print(result)
