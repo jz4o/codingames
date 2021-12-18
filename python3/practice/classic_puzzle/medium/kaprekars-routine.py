cf230f579eb764b2a3c519240025ac1630841699
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-18 22:24:19 +0900

    add python3/Kaprekar's routine

---
+++ python3/practice/classic_puzzle/medium/kaprekars-routine.py
@@ -0,0 +1,30 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n_1 = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+digits_size = len(n_1)
+answers = []
+
+temp = n_1
+while temp not in answers:
+    answers.append(temp)
+
+    biggest = int(''.join(reversed(sorted(temp))))
+    smallest = int(''.join(sorted(temp)))
+
+    answer = biggest - smallest
+
+    temp = str(answer).zfill(digits_size)
+
+begin_index = answers.index(temp)
+result = ' '.join(answers[begin_index:])
+
+# print("1 2 3 4")
+print(result)
