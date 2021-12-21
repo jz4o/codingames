0307f05d7e7849b6b4fc4d7c6272249520374555
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-22 00:45:31 +0900

    refactor python3/1D Spreadsheet

--- python3/practice/classic_puzzle/easy/1d-spreadsheet.py
+++ python3/practice/classic_puzzle/easy/1d-spreadsheet.py
@@ -4,54 +4,62 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+class SpreadSheet:
+    def __init__(self):
+        self.cells = []
+
+    def get_cell(self, index):
+        return self.cells[index]
+
+    def add_cell(self, cell):
+        self.cells.append(cell)
+        cell.spread_sheet = self
+
+
 class Cell:
-    def __init__(self, operation, arg1, arg2, value):
+    def __init__(self, operation, arg1, arg2):
         self.operation = operation
         self.arg1 = arg1
         self.arg2 = arg2
-        self.value = value
+        self.spread_sheet = None
+        self.value = None
 
+    def get_arg_value(self, arg):
+        if '$' in arg:
+            return self.spread_sheet.get_cell(int(arg[1:])).get_value()
+        elif arg == '_':
+            return '0'
+        else:
+            return arg
 
-def get_cell_value(cells, index):
-    value = cells[index].value
-    return value if value is not None else calc(cells, index)
+    def get_value(self):
+        return self.value if self.value is not None else self.calc_value()
 
+    def calc_value(self):
+        arg1 = int(self.get_arg_value(self.arg1))
+        arg2 = int(self.get_arg_value(self.arg2))
 
-def get_arg_value(cells, arg):
-    if '$' in arg:
-        return get_cell_value(cells, int(arg[1:]))
-    elif arg == '_':
-        return '0'
-    else:
-        return arg
+        result = 0
+        if self.operation == 'VALUE':
+            result = arg1
+        elif self.operation == 'ADD':
+            result = arg1 + arg2
+        elif self.operation == 'SUB':
+            result = arg1 - arg2
+        elif self.operation == 'MULT':
+            result = arg1 * arg2
 
+        self.value = str(result)
 
-def calc(cells, index):
-    cell = cells[index]
-    arg1 = int(get_arg_value(cells, cell.arg1))
-    arg2 = int(get_arg_value(cells, cell.arg2))
-
-    result = 0
-    if cell.operation == 'VALUE':
-        result = arg1
-    elif cell.operation == 'ADD':
-        result = arg1 + arg2
-    elif cell.operation == 'SUB':
-        result = arg1 - arg2
-    elif cell.operation == 'MULT':
-        result = arg1 * arg2
-
-    cells[index].value = str(result)
-
-    return cells[index].value
+        return self.value
 
 
 n = int(input())
-cells = []
+spread_sheet = SpreadSheet()
 for i in range(n):
     operation, arg_1, arg_2 = input().split()
 
-    cells.append(Cell(operation, arg_1, arg_2, None))
+    spread_sheet.add_cell(Cell(operation, arg_1, arg_2))
 
 for i in range(n):
 
@@ -59,4 +67,4 @@ for i in range(n):
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
     # print("1")
-    print(get_cell_value(cells, i))
+    print(spread_sheet.get_cell(i).get_value())
