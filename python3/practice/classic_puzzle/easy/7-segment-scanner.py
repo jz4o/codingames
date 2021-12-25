4fceedb0a7cd7dc12304e6dd599605368b1390ff
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-25 23:48:47 +0900

    add python3/7-segment scanner

---
+++ python3/practice/classic_puzzle/easy/7-segment-scanner.py
@@ -0,0 +1,39 @@
+import sys
+import math
+
+import pandas as pd
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+line_1 = input()
+line_2 = input()
+line_3 = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+DIGIT_NUMS = [
+    ' ||_ _ ||',
+    '       ||',
+    '  |___ | ',
+    '   ___ ||',
+    ' |  _  ||',
+    ' | ___  |',
+    ' ||___  |',
+    '   _   ||',
+    ' ||___ ||',
+    ' | ___ ||'
+]
+
+transposed_lines = pd.DataFrame([list(line_1), list(line_2), list(line_3)]).T
+
+results = []
+for index in range(0, len(transposed_lines), 3):
+    num = DIGIT_NUMS.index(''.join(transposed_lines.values[index:index+3].flatten()))
+    results.append(num)
+
+result = ''.join(map(str, results))
+
+# print("number")
+print(result)
