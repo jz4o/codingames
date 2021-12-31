326566c72e02fe9b82a0324ece2cce0358441c39
jz4o <26317360+jz4o@users.noreply.github.com>
2021-12-31 22:26:29 +0900

    add python3/The greatest number

---
+++ python3/practice/classic_puzzle/hard/the-greatest-number.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+_input = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+minus_flag = '-' in _input
+dot_flag = '.' in _input
+_input = _input.replace('-', '').replace('.', '').replace(' ', '')
+
+if minus_flag:
+    _input = ''.join(sorted(list(_input)))
+    if dot_flag:
+        _input = _input[0] + '.' + _input[1:]
+    _input = '-' + _input
+    if re.match('^-0\.0+$', _input):
+        _input = '0'
+else:
+    _input = ''.join(sorted(list(_input), reverse = True))
+    if dot_flag:
+        _input = _input[:-1] + '.' + _input[-1]
+    if _input.endswith('.0'):
+        _input = _input[:-2]
+
+# print("greatest")
+print(_input)
