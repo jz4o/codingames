b2182b5f6394ec600c8244bf9a46e376b93b885c
jz4o <26317360+jz4o@users.noreply.github.com>
2022-01-06 19:09:44 +0900

    add python3/Benford's law

---
+++ python3/practice/classic_puzzle/easy/benfords-law.py
@@ -0,0 +1,33 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+transactions = []
+for i in range(n):
+    transaction = input()
+    transactions.append(transaction)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+benford_law_percentages = [0, 0.301, 0.176, 0.125, 0.097, 0.079, 0.067, 0.058, 0.051, 0.046]
+first_numbers = list(map(lambda transaction: int(re.findall('[0-9]', transaction)[0]), transactions))
+
+is_fraudulent = False
+for i in range(1, 9 + 1):
+    benford_law_percentage = benford_law_percentages[i]
+    percent = first_numbers.count(i) / n
+
+    if percent < benford_law_percentage - 0.1 or benford_law_percentage + 0.1 < percent:
+        is_fraudulent = True
+        break
+
+result = str(is_fraudulent).lower()
+
+# print("false")
+print(result)
