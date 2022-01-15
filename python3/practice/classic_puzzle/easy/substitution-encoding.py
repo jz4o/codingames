8b01d365927498f08a0a5f1fd0a6504c98c55a17
jz4o <26317360+jz4o@users.noreply.github.com>
2022-01-15 22:28:23 +0900

    add python3/Substitution Encoding

---
+++ python3/practice/classic_puzzle/easy/substitution-encoding.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+rows = int(input())
+grid = []
+for i in range(rows):
+    row = input()
+    grid.append(row.split(' '))
+message = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+replace_table = {}
+for row_index, row in enumerate(grid):
+    for column_index, char in enumerate(row):
+        replace_table[char] = f'{row_index}{column_index}'
+
+result = ''.join([replace_table[char] for char in message])
+
+# print("encode message")
+print(result)
