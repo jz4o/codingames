868067484fe713d3b2bbb8b3944d133a94055bee
jz4o <26317360+jz4o@users.noreply.github.com>
2022-01-16 23:16:10 +0900

    add python3/Count of Primes in a number grid

---
+++ python3/practice/classic_puzzle/medium/count-of-primes-in-a-number-grid.py
@@ -0,0 +1,51 @@
+import sys
+import math
+
+import pandas as pd
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+r, c = [int(i) for i in input().split()]
+digit_grid = []
+for i in range(r):
+    row = input()
+    digit_grid.append(row.split(' '))
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+def combination_digits(row):
+    result = []
+    for size in range(2, len(row) + 1):
+        for index in range(len(row) - size + 1):
+            result.append(int(''.join(row[index:index + size])))
+
+    return result
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+
+    i = 2
+    while i < num:
+        if num % i == 0:
+            return False
+
+        i += 1
+
+    return True
+
+
+digits = []
+for row in digit_grid:
+    digits.extend([int(i) for i in row])
+    digits.extend(combination_digits(row))
+for row in pd.DataFrame(digit_grid).T.values:
+    digits.extend(combination_digits(row))
+
+result = len([digit for digit in set(digits) if is_prime(digit)])
+
+# print("answer")
+print(result)
