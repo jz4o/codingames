25b96946f4760c84c1367fda76e0d4481c6ca964
jz4o <26317360+jz4o@users.noreply.github.com>
2022-01-22 21:51:20 +0900

    add python3/Next growing number

---
+++ python3/practice/classic_puzzle/easy/next-growing-number.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+digits = list(map(int, str(int(n) + 1)))
+
+increment_index = [index for index in range(len(digits) - 1) if digits[index] > digits[index + 1]][0]
+increment_number = digits[increment_index]
+for index in range(increment_index + 1, len(digits)):
+    digits[index] = increment_number
+
+result = ''.join(map(str, digits))
+
+# print("answer")
+print(result)
