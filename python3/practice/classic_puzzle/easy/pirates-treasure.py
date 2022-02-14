6a158883e2294654b181f6a3c13c1f6ef3a963f9
jz4o <26317360+jz4o@users.noreply.github.com>
2022-02-14 23:49:13 +0900

    add python3/Pirate's treasure

---
+++ python3/practice/classic_puzzle/easy/pirates-treasure.py
@@ -0,0 +1,45 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+w = int(input())
+h = int(input())
+grid = []
+grid.append([1] * (w + 2))
+for i in range(h):
+    row = []
+
+    row.append(1)
+    for j in input().split():
+        v = int(j)
+        row.append(v)
+    row.append(1)
+
+    grid.append(row)
+grid.append([1] * (w + 2))
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+def find_pirates_treasure_indexes(grid):
+    for row_index, row in enumerate(grid[1:-1], start = 1):
+        for column_index, column in enumerate(row[1:-1], start = 1):
+            if 0 in grid[row_index - 1][column_index-1:column_index+2]:
+                continue
+            if row[column_index - 1] == 0:
+                continue
+            if column != 0:
+                continue
+            if row[column_index + 1] == 0:
+                continue
+            if 0 in grid[row_index + 1][column_index - 1:column_index + 2]:
+                continue
+
+            return f'{column_index - 1} {row_index - 1}'
+
+result = find_pirates_treasure_indexes(grid)
+
+# print("x y")
+print(result)
