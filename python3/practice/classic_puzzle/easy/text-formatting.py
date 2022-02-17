a6600b048318df74fd88b8c0d45a29e43e009911
jz4o <26317360+jz4o@users.noreply.github.com>
2022-02-17 23:52:32 +0900

    add python3/Text formatting

---
+++ python3/practice/classic_puzzle/easy/text-formatting.py
@@ -0,0 +1,30 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+intext = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+result = intext
+
+require_period = result.endswith('.')
+
+result = result.replace(',', ', ')
+result = re.sub('\\s+', ' ', result)
+
+result = '. '.join([line.strip().capitalize() for line in result.split('.') if line != ''])
+
+result = result.replace(' ,', ',')
+result = re.sub(',+', ',', result)
+
+if require_period:
+    result += '.'
+
+# print("solution")
+print(result)
