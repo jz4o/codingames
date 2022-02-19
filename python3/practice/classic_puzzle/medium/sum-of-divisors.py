829b9c8d7cf437e9fc9e7a6aa657e6be373ea004
jz4o <26317360+jz4o@users.noreply.github.com>
2022-02-19 23:44:16 +0900

    add python3/Sum of divisors

---
+++ python3/practice/classic_puzzle/medium/sum-of-divisors.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+result = sum([n // i * i for i in range(1, n + 1)])
+
+# print("sum")
+print(result)
