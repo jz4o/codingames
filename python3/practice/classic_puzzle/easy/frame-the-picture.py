efb21827476a1225103cb0cc6f14c29de2fb6028
jz4o <26317360+jz4o@users.noreply.github.com>
2022-03-06 22:30:24 +0900

    add python3/Frame the picture

---
+++ python3/practice/classic_puzzle/easy/frame-the-picture.py
@@ -0,0 +1,35 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+frame_pattern = input()  # the ASCII art pattern to use to frame the picture
+# h: the height of the picture
+# w: the width  of the picture
+h, w = [int(i) for i in input().split()]
+lines = []
+for i in range(h):
+    line = input()  # the ASCII art picture line by line
+
+    lines.append(line)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+def surround(grid, char):
+    return [
+        [char] * (len(grid[0]) + 2),
+        *[[char, *row, char] for row in grid],
+        [char] * (len(grid[0]) + 2)
+    ]
+
+grid = [list(line.ljust(w)) for line in lines]
+for frame_char in reversed(frame_pattern + ' '):
+    grid = surround(grid, frame_char)
+
+results = [''.join(row) for row in grid]
+
+# print("Write framed picture line by line here")
+for result in results:
+    print(result)
