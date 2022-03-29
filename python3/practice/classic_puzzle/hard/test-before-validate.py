fb9d6ec67f78eb9ffc33ded993ee2c7115c5bffd
jz4o <26317360+jz4o@users.noreply.github.com>
2022-03-29 21:34:38 +0900

    add python3/Test before Validate!

---
+++ python3/practice/classic_puzzle/hard/test-before-validate.py
@@ -0,0 +1,55 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+actions = []
+for i in range(n):
+    action = input()
+    actions.append(action)
+nb_orders = int(input())
+orders = []
+for i in range(nb_orders):
+    order = input()
+    orders.append(order)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+require_actions = {}
+for order in orders:
+    first, second, third = order.split()[:3]
+    before, after = [first, third] if second == 'before' else [third, first]
+
+    if after not in require_actions:
+        require_actions[after] = []
+    require_actions[after].append(before)
+
+available_actions = list(set(actions) - set(require_actions.keys()))
+
+temp_actions = actions
+
+results = []
+while len(temp_actions) > 0:
+    action = [action for action in temp_actions if action in available_actions][0]
+
+    available_actions.remove(action)
+    temp_actions.remove(action)
+
+    for v in require_actions.values():
+        if action in v:
+            v.remove(action)
+
+    unlock_actions = [k for k, v in require_actions.items() if len(v) == 0]
+
+    available_actions.extend(unlock_actions)
+    for unlock_action in unlock_actions:
+        require_actions.pop(unlock_action)
+
+    results.append(action)
+
+# print("answer")
+for result in results:
+    print(result)
