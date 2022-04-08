8d49e135baf6292309bb661f3fa790ca3fc9634d
jz4o <26317360+jz4o@users.noreply.github.com>
2022-04-08 23:04:24 +0900

    add python3/Horse-racing Hyperduals

---
+++ python3/practice/classic_puzzle/easy/horse-racing-hyperduals.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+strengths = []
+for i in range(n):
+    v, e = [int(j) for j in input().split()]
+    strengths.append({'v': v, 'e': e})
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+distances = []
+for index, left in enumerate(strengths, start=1):
+    for right in strengths[index:]:
+        distance = abs(right['v'] - left['v']) + abs(right['e'] - left['e'])
+        distances.append(distance)
+
+result = min([distance for distance in distances if distance >= 0])
+
+# print("42")
+print(result)
