9233b26a5f82f1936f7a761dca446779d371789c
jz4o <26317360+jz4o@users.noreply.github.com>
2022-04-19 22:37:03 +0900

    add ruby/Walk on a Die

---
+++ ruby/practice/classic_puzzle/easy/walk-on-a-die.rb
@@ -0,0 +1,53 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+lines = []
+3.times do
+  line = gets.chomp # One line out of three in the string describing the starting position.
+  lines << line
+end
+commands = gets.chomp # The sequence of ULDR-characters describing the steps to perform.
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+class Dice
+  attr_reader :on
+
+  def initialize(numbers_string)
+    @front, @left, @on, @right, @opposite, @behind = numbers_string.chars
+  end
+
+  def move!(commands)
+    commands.chars.each { |command| method(command.to_sym).call }
+  end
+
+  def move_to_forward!
+    @front, @on, @opposite, @behind = @opposite, @front, @behind, @on
+  end
+  alias_method :U, :move_to_forward!
+
+  def move_to_backward!
+    @front, @left, @on, @right, @opposite, @behind = @opposite, @right, @behind, @left, @front, @on
+  end
+  alias_method :D, :move_to_backward!
+
+  def move_to_left!
+    @front, @left, @on, @right, @opposite, @behind = @opposite, @behind, @left, @front, @right, @on
+  end
+  alias_method :L, :move_to_left!
+
+  def move_to_right!
+    @front, @left, @on, @right, @opposite, @behind = @opposite, @front, @right, @behind, @left, @on
+  end
+  alias_method :R, :move_to_right!
+end
+
+dice = Dice.new lines.join.delete(' ')
+dice.move! commands
+
+result = dice.on
+
+# The number on the side you end up on after having performed `commands`.
+# puts "number"
+puts result
