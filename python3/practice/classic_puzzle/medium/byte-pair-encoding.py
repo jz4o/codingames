7de581e99859ce7e4522ae6814ac874fea0ef59d
jz4o <26317360+jz4o@users.noreply.github.com>
2022-05-02 22:08:57 +0900

    add python3/Byte pair encoding

---
+++ python3/practice/classic_puzzle/medium/byte-pair-encoding.py
@@ -0,0 +1,45 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n, m = [int(i) for i in input().split()]
+lines = []
+for i in range(n):
+    line = input()
+    lines.append(line)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+string = ''.join(lines)
+
+replaced_string = string
+replace_list = []
+
+replace_char = 'Z'
+
+while True:
+    chars = list(replaced_string)
+    char_groups = [f'{l}{r}' for l, r in zip(chars[:-1], chars[1:])]
+    group_sizes = [[char_group, len(re.findall(char_group, replaced_string))] for char_group in char_groups]
+
+    max_group_size = max([group_size for _, group_size in group_sizes])
+    target_char, target_size = [[char_group, group_size] for char_group, group_size in group_sizes if group_size == max_group_size][0]
+
+    if target_size < 2:
+        break
+
+    replaced_string = replaced_string.replace(target_char, replace_char)
+    replace_list.append(f'{replace_char} = {target_char}')
+
+    replace_char = chr(ord(replace_char) - 1)
+
+results = [replaced_string] + replace_list
+
+# print("answer")
+for result in results:
+    print(result)
