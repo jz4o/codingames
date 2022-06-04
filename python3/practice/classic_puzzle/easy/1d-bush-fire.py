c86090800ae38d1bd4c39d3a1938c42e0738955e
jz4o <26317360+jz4o@users.noreply.github.com>
2022-06-04 22:48:22 +0900

    add python3/1D Bush Fire

---
+++ python3/practice/classic_puzzle/easy/1d-bush-fire.py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+lines = []
+for i in range(n):
+    line = input()
+    lines.append(line)
+for i in range(n):
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    line = lines.pop(0)
+    indexes = [index for index, char in enumerate(line) if char == 'f']
+
+    drop_count = 0
+    while len(indexes) > 0:
+        index = indexes.pop(0)
+
+        drop_count += 1
+
+        indexes = [i for i in indexes if i > index + 2]
+
+    result = drop_count
+
+    # print("answer")
+    print(result)
