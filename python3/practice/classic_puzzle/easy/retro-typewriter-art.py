c14c4d1df8201a05d40bea1d08e6919debe66456
jz4o <26317360+jz4o@users.noreply.github.com>
2022-07-10 23:19:53 +0900

    add python3/Retro Typewriter Art

---
+++ python3/practice/classic_puzzle/easy/retro-typewriter-art.py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+t = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+ABBREVIATIONS = {
+    'sp': ' ',
+    'bS': '\\',
+    'sQ': "'",
+    'nl': "\n"
+}
+
+t = t.replace('nl', '1nl')
+
+chars = []
+for chunk in t.split():
+    count, char = re.findall('^(\d+)(.+?)$', chunk)[0]
+    chars.append((ABBREVIATIONS[char] if char in ABBREVIATIONS else char) * int(count))
+
+result = ''.join(chars)
+
+# print("answer")
+print(result)
