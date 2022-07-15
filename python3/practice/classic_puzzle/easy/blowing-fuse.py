a8cc227458fe2474418c35c36ac67a374ef6f157
jz4o <26317360+jz4o@users.noreply.github.com>
2022-07-15 22:15:04 +0900

    add python3/Blowing Fuse

---
+++ python3/practice/classic_puzzle/easy/blowing-fuse.py
@@ -0,0 +1,56 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n, m, c = [int(i) for i in input().split()]
+nxs = []
+for i in input().split():
+    nx = int(i)
+    nxs.append(nx)
+mxs = []
+for i in input().split():
+    mx = int(i)
+    mxs.append(mx)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+class Device:
+    def __init__(self, ampere):
+        self.ampere = ampere
+        self.status = 'off'
+
+    def switch(self):
+        if self.status == 'on':
+            self.status = 'off'
+            return -self.ampere
+        else:
+            self.status = 'on'
+            return self.ampere
+
+
+devices = [Device(nx) for nx in nxs]
+devices.insert(0, Device(0))
+
+sum_ampere = 0
+sum_amperes = []
+for mx in mxs:
+    device = devices[mx]
+    sum_ampere += device.switch()
+
+    sum_amperes.append(sum_ampere)
+
+results = []
+if max(sum_amperes) > c:
+    results.append('Fuse was blown.')
+else:
+    results.append('Fuse was not blown.')
+    results.append(f'Maximal consumed current was {max(sum_amperes)} A.')
+
+# print("Fuse was not blown.")
+# print("Maximal consumed current was XX A.")
+for result in results:
+    print(result)
