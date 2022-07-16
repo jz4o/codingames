4472337822c7045d8c2b1ce24f9c6a17d216802a
jz4o <26317360+jz4o@users.noreply.github.com>
2022-07-16 23:27:33 +0900

    add python3/Detective Geek

---
+++ python3/practice/classic_puzzle/easy/detective-geek.py
@@ -0,0 +1,36 @@
+# import sys
+# import math
+
+import numpy as np
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+time = input()
+address = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+formatted_time = str(int(time.replace('*', '0').replace('#', '1'), 2)).rjust(4, '0')
+formatted_time = formatted_time[:2] + ':' + formatted_time[-2:]
+
+decrypted_address = ''
+for word in address.split():
+    month_chars = ''
+    for month in re.findall('.{3}', word):
+        month_index = MONTHS.index(month)
+        month_chars += np.base_repr(month_index, len(MONTHS))
+
+    decrypted_address += chr(int(month_chars, len(MONTHS)))
+
+results = []
+results.append(formatted_time)
+results.append(decrypted_address)
+
+# print("answer")
+for result in results:
+    print(result)
