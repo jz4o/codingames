d5d4001a5c48ec5f9ac486d112b3831c1dd36f9a
jz4o <26317360+jz4o@users.noreply.github.com>
2022-08-05 22:27:19 +0900

    add python3/Smooth!

---
+++ python3/practice/classic_puzzle/easy/smooth.py
@@ -0,0 +1,26 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+fs = []
+for i in range(n):
+    f = int(input())
+    fs.append(f)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+results = []
+for f in fs:
+    for i in [5, 3, 2]:
+        while f % i == 0:
+            f /= i
+
+    results.append('VICTORY' if f == 1 else 'DEFEAT')
+
+# print("DEFEAT")
+for result in results:
+    print(result)
