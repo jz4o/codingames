f6508a6ba976896b81ae65f7ce84149137580fa0
jz4o <26317360+jz4o@users.noreply.github.com>
2022-08-07 18:27:47 +0900

    add python3/Someone's Acting Sus....

---
+++ python3/practice/classic_puzzle/easy/someones-acting-sus----.py
@@ -0,0 +1,41 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+class Log:
+    def __init__(self, time, room_index):
+        self.time = time
+        self.room_index = room_index
+
+
+l = int(input())
+f = input()
+n, k = [int(i) for i in input().split()]
+crewmates = []
+for i in range(n):
+    crewmate = input()
+    crewmates.append(crewmate)
+for i in range(n):
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    crewmate = crewmates[i]
+    logs = [Log(index, f.index(char)) for index, char in enumerate(crewmate) if char in f]
+
+    is_sus = False
+    for l, r in zip(logs[:-1], logs[1:]):
+        time = r.time - l.time
+        distance = abs(r.room_index - l.room_index)
+        distance = min(distance, len(f) - distance)
+
+        if time < distance:
+            is_sus = True
+            break
+
+    result = 'SUS' if is_sus else 'NOT SUS'
+
+    # print("answer")
+    print(result)
