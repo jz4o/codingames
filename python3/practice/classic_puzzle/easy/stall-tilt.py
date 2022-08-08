01b56d2bfd237a68bd486a74c487d79a4e9da50f
jz4o <26317360+jz4o@users.noreply.github.com>
2022-08-08 14:16:04 +0900

    add python3/Stall tilt

---
+++ python3/practice/classic_puzzle/easy/stall-tilt.py
@@ -0,0 +1,56 @@
+# import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+v = int(input())
+speeds = []
+for i in range(n):
+    speed = int(input())
+    speeds.append(speed)
+bends = []
+for i in range(v):
+    bend = int(input())
+    bends.append(bend)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+class Motorcycle:
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed
+
+
+def motorcycle_speed(motorcycle):
+    return motorcycle.speed
+
+
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
+motorcycles = [Motorcycle(alphabets[index], speed) for index, speed in enumerate(speeds)]
+staled_motorcycles = []
+
+border_tan = math.tan(60 * math.pi / 180)
+G = 9.81
+border_speeds = []
+for bend in bends:
+    border_speed = math.sqrt(border_tan * bend * G)
+    border_speeds.append(border_speed)
+
+    bend_staled_motorcycles = [motorcycle for motorcycle in motorcycles if border_speed < motorcycle.speed]
+    motorcycles = [motorcycle for motorcycle in motorcycles if motorcycle not in bend_staled_motorcycles]
+
+    staled_motorcycles = sorted(bend_staled_motorcycles, key=motorcycle_speed, reverse=True) + staled_motorcycles
+
+results = []
+results.append(int(min(border_speeds)))
+results.append('y')
+results.extend([motorcycle.name for motorcycle in sorted(motorcycles, key=motorcycle_speed, reverse=True)])
+results.extend([motorcycle.name for motorcycle in staled_motorcycles])
+
+# print("answer")
+for result in results:
+    print(result)
