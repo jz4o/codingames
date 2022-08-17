a0d060aa52ee0408a6a35e0cf3882d3580500817
jz4o <26317360+jz4o@users.noreply.github.com>
2022-08-17 23:50:00 +0900

    add python3/Distributing Candy

---
+++ python3/practice/classic_puzzle/easy/distributing-candy.py
@@ -0,0 +1,21 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n, m = [int(i) for i in input().split()]
+xs = []
+for i in input().split():
+    x = int(i)
+    xs.append(x)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+xs = list(sorted(xs))
+
+result = min([xs[i + m - 1] - xs[i] for i in range(len(xs) - m + 1)])
+
+# print("answer")
+print(result)
