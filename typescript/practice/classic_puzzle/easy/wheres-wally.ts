2faf00c1738789b8a83dc9a68050e149d8cf08cc
jz4o <26317360+jz4o@users.noreply.github.com>
2023-08-18 23:51:28 +0900

    add typescript/Where's Wally

---
+++ typescript/practice/classic_puzzle/easy/wheres-wally.ts
@@ -0,0 +1,50 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const wallyWidth: number = parseInt(inputs[0]);
+const wallyHeight: number = parseInt(inputs[1]);
+const wallyRows: string[] = [];
+for (let i = 0; i < wallyHeight; i++) {
+    const wallyRow: string = readline();
+    wallyRows.push(wallyRow);
+}
+var inputs: string[] = readline().split(' ');
+const pictureWidth: number = parseInt(inputs[0]);
+const pictureHeight: number = parseInt(inputs[1]);
+const pictureRows: string[] = [];
+for (let i = 0; i < pictureHeight; i++) {
+    const pictureRow: string = readline();
+    pictureRows.push(pictureRow);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const wally: string = wallyRows.join('');
+
+const wallyIndexes: number[] = wally
+    .split('')
+    .map((char, index) => char !== ' ' ? index : undefined)
+    .filter(index => index !== undefined);
+const wallyChars: string = wally.replaceAll(' ', '');
+
+const pictureGrid: string[][] = pictureRows.map(pictureRow => pictureRow.split(''));
+
+let result: string;
+[...Array(pictureHeight - wallyHeight + 1).keys()].some(h => {
+    return [...Array(pictureWidth - wallyWidth + 1).keys()].some(w => {
+        const tempPictureChars: string[] = pictureGrid.slice(h, h + wallyHeight).flatMap(pictureRow => pictureRow.slice(w, w + wallyWidth));
+        const pictureChars: string = wallyIndexes.map(index => tempPictureChars[index]).join('');
+        if (wallyChars !== pictureChars) {
+            return false;
+        }
+
+        return result = `${w} ${h}`;
+    });
+});
+
+// console.log('x y');
+console.log(result);
