7951bbb03020a7ce0b12e541178ae2fb30bc55b8
jz4o <26317360+jz4o@users.noreply.github.com>
2023-09-05 23:47:11 +0900

    add typescript/The Mystic Rectangle

---
+++ typescript/practice/classic_puzzle/easy/the-mystic-rectangle.ts
@@ -0,0 +1,36 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const x: number = parseInt(inputs[0]);
+const y: number = parseInt(inputs[1]);
+var inputs: string[] = readline().split(' ');
+const u: number = parseInt(inputs[0]);
+const v: number = parseInt(inputs[1]);
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const WIDTH: number = 200;
+const HEIGHT: number = 150;
+const xDistance: number = Math.min(Math.abs(x - u), WIDTH - Math.abs(x - u));
+const yDistance: number = Math.min(Math.abs(y - v), HEIGHT - Math.abs(y - v));
+
+const DIAGONAL_MOVE_SPEED: number = 0.5;
+const diagonalDistance: number = Math.min(xDistance, yDistance);
+const diagonalMoveTime: number = diagonalDistance * DIAGONAL_MOVE_SPEED;
+
+const X_MOVE_SPEED: number = 0.3;
+const Y_MOVE_SPEED: number = 0.4;
+const cardinalDistance: number = Math.abs(yDistance - xDistance);
+const cardinalMoveSpeed: number = (xDistance < yDistance ? Y_MOVE_SPEED : X_MOVE_SPEED);
+const cardinalMoveTime: number = cardinalDistance * cardinalMoveSpeed;
+
+let moveTime: number = diagonalMoveTime + cardinalMoveTime;
+
+const result: string = moveTime.toFixed(1);
+
+// console.log('0.0');
+console.log(result);
