d300eb1942d6178400d32fdbf144b2e82d5719e2
jz4o <26317360+jz4o@users.noreply.github.com>
2023-09-09 15:48:32 +0900

    add typescript/Random Walk

---
+++ typescript/practice/classic_puzzle/easy/random-walk.ts
@@ -0,0 +1,45 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const a: number = parseInt(readline());
+const b: number = parseInt(readline());
+const m: number = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const [UP, DOWN, LEFT, RIGHT]: number[] = [...Array(4).keys()];
+
+// D[0] = 0
+let d: number = 0;
+
+let u: number = 0;
+let r: number = 0;
+let step: number = 0;
+while (true) {
+    step++;
+
+    d = (a * d + b) % m;
+
+    const direction: number = d % 4;
+    if (direction === UP) {
+        u++;
+    } else if (direction === DOWN) {
+        u--;
+    } else if (direction === LEFT) {
+        r--;
+    } else if (direction === RIGHT) {
+        r++;
+    }
+
+    if (u === 0 && r === 0) {
+        break;
+    }
+}
+
+const result: string = step.toString();
+
+// console.log('result');
+console.log(result);
