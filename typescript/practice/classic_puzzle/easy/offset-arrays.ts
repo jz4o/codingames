87c956da1a3c83dde77d995f18eca76ebd7e627b
jz4o <26317360+jz4o@users.noreply.github.com>
2023-09-10 11:00:55 +0900

    add typescript/Offset Arrays

---
+++ typescript/practice/classic_puzzle/easy/offset-arrays.ts
@@ -0,0 +1,56 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n: number = parseInt(readline());
+const assignments: string[] = [];
+for (let i = 0; i < n; i++) {
+    const assignment: string = readline();
+    assignments.push(assignment);
+}
+const x: string = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const zip: Function = <T>(a: T[], b: T[]): T[][] => {
+    const arraySize: number = Math.max(a.length, b.length);
+    return [...Array(arraySize).keys()].map(index => {
+        return [a[index], b[index]];
+    });
+};
+
+const rangeArrayFromTo: Function = (from: number, to: number): number[] => {
+    return [...Array(to - from + 1).keys()].map(i => i + from);
+}
+
+const assignmentRegexp: RegExp = /([A-Z]+)\[([\-\d]+)\.\.([\-\d]+)\]\s=\s([\-\s\d]+)/;
+const assignmentObject: { [key: string]: { [key: string]: string } } = {};
+assignments.forEach(assignment => {
+    const [_, arrayName, strRangeBegin, strRangeEnd, strValues]: string[] = assignmentRegexp.exec(assignment);
+    const rangeBegin: number = parseInt(strRangeBegin);
+    const rangeEnd: number = parseInt(strRangeEnd);
+    const values: string[] = strValues.split(' ');
+
+    if (!(arrayName in assignmentObject)) {
+        assignmentObject[arrayName] = {};
+    }
+
+    const targetObject: { [key: string]: string } = assignmentObject[arrayName];
+    zip(rangeArrayFromTo(rangeBegin, rangeEnd), values).forEach(row => {
+        const [index, value]: [number, string] = row;
+        targetObject[index.toString()] = value;
+    });
+});
+
+const resultRegexp: RegExp = /([A-Z]+)\[([\-\d]+)\]/;
+let result: string = x.toString();
+while (result.match(resultRegexp)) {
+    const [_, arrayName, key]: string[] = resultRegexp.exec(result);
+
+    result = result.replace(`${arrayName}[${key}]`, assignmentObject[arrayName][key]);
+}
+
+// console.log('0');
+console.log(result);
