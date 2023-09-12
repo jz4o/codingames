fe74abecffb90621278e52007007b5d7df0de119
jz4o <26317360+jz4o@users.noreply.github.com>
2023-09-12 23:52:38 +0900

    add typescript/Rotating arrows

---
+++ typescript/practice/classic_puzzle/easy/rotating-arrows.ts
@@ -0,0 +1,89 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const W: number = parseInt(inputs[0]);
+const H: number = parseInt(inputs[1]);
+var inputs: string[] = readline().split(' ');
+const x: number = parseInt(inputs[0]);
+const y: number = parseInt(inputs[1]);
+const lines: string[] = [];
+for (let i = 0; i < H; i++) {
+    const line: string = readline(); // The line of w arrows represented by ascii char ^v<>
+    lines.push(line);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+class Position {
+    y: number;
+    x: number;
+    value: string;
+
+    constructor(y: number, x: number, value: string) {
+        this.y = y;
+        this.x = x;
+        this.value = value;
+    }
+
+    arrowPosition: Function = (): [number, number] => {
+        if (this.value === '^') {
+            return [this.y - 1, this.x];
+        } else if (this.value === '>') {
+            return [this.y, this.x + 1];
+        } else if (this.value === 'v') {
+            return [this.y + 1, this.x];
+        } else if (this.value === '<') {
+            return [this.y, this.x - 1];
+        } else {
+            return [0, 0];
+        }
+    };
+
+    rotateArrow: Function = () => {
+        const arrows: string[] = ['^', '>', 'v', '<'];
+        const arrowIndex: number = arrows.indexOf(this.value);
+        const nextArrow: string = arrows[(arrowIndex + 1) % arrows.length];
+
+        this.value = nextArrow;
+    };
+
+    isEqualPosition: Function = (y: number, x: number): boolean => {
+        return this.y === y && this.x === x;
+    };
+
+    isOverFlow: Function = (): boolean => {
+        return this.value === 'X';
+    };
+}
+
+const charGrid: string[][] = lines.map(line => `X${line}X`.split(''));
+charGrid.unshift(Array(W + 2).fill('X'));
+charGrid.push(Array(W + 2).fill('X'));
+
+const grid: Position[][] = charGrid.map((charRow, rowIndex) => {
+    return charRow.map((value, valueIndex) => new Position(rowIndex, valueIndex, value));
+});
+
+let times: number = 0;
+let currentPosition: Position = grid[y + 1][x + 1];
+while (true) {
+    times++;
+
+    currentPosition.rotateArrow();
+    const [arrowY, arrowX]: number[] = currentPosition.arrowPosition();
+
+    currentPosition = grid[arrowY][arrowX];
+
+    if (currentPosition.isEqualPosition(y + 1, x + 1) || currentPosition.isOverFlow()) {
+        break;
+    }
+}
+
+const result: string = times.toString();
+
+// console.log('0');
+console.log(result);
