9d14d8656adc5a3423b4dfa76215030547fe2cdc
jz4o <26317360+jz4o@users.noreply.github.com>
2023-09-17 17:44:22 +0900

    add typescript/Robot Reach

---
+++ typescript/practice/classic_puzzle/easy/robot-reach.ts
@@ -0,0 +1,102 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const R: number = parseInt(readline());
+const C: number = parseInt(readline());
+const T: number = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+class Grid {
+    rows: Row[];
+
+    constructor() {
+        this.rows = [];
+    }
+
+    addRow: Function = (row: Row): void => {
+        this.rows.push(row);
+    };
+
+    checkReachableCells: Function = (threshold: number): void => {
+        const bottomRow: Row = this.rows[this.rows.length - 1];
+        const bottomRightCell: Cell = bottomRow.cells[bottomRow.cells.length - 1];
+
+        this.checkReachableCell(bottomRightCell.y, bottomRightCell.x, threshold);
+    };
+
+    checkReachableCell: Function = (targetY: number, targetX: number, threshold: number): boolean => {
+        const targetCell: Cell = this.rows[targetY].cells[targetX];
+
+        if (targetCell.isReachable !== undefined) {
+            return targetCell.isReachable;
+        }
+
+        const accessibleValue: boolean = targetCell.value <= threshold;
+        const isUpperLeftCell: boolean = targetCell.y === 0 && targetCell.x === 0;
+        const topCellIsReachable: boolean = targetCell.y !== 0 ? this.checkReachableCell(targetCell.y - 1, targetCell.x, threshold) : false;
+        const leftCellIsReachable: boolean = targetCell.x !== 0 ? this.checkReachableCell(targetCell.y, targetCell.x - 1, threshold) : false;
+
+        const isReachableCell: boolean = isUpperLeftCell || topCellIsReachable || leftCellIsReachable;
+        this.rows[targetY].cells[targetX].isReachable = accessibleValue && isReachableCell;
+
+        return this.rows[targetY].cells[targetX].isReachable;
+    };
+
+    countReachableCell: Function = (): number => {
+        return this.rows.flatMap(row => row.cells).filter(cell => cell.isReachable).length;
+    };
+}
+
+class Row {
+    cells: Cell[];
+
+    constructor() {
+        this.cells = [];
+    }
+
+    addCell: Function = (cell: Cell): void => {
+        this.cells.push(cell);
+    }
+}
+
+class Cell {
+    y: number;
+    x: number;
+    value: number;
+    isReachable: boolean;
+
+    constructor(y: number, x: number, value: number) {
+        this.y = y;
+        this.x = x;
+        this.value = value;
+    }
+}
+
+const sumRIndexDigits: number[] = [...Array(R).keys()].map(index => {
+    return index.toString().split('').map(char => parseInt(char)).reduce((sum, i) => sum + i);
+});
+const sumCIndexDigits: number[] = [...Array(C).keys()].map(index => {
+    return index.toString().split('').map(char => parseInt(char)).reduce((sum, i) => sum + i);
+});
+
+const grid: Grid = new Grid();
+sumRIndexDigits.forEach((rowSum, rowIndex) => {
+    const row: Row = new Row();
+    sumCIndexDigits.forEach((columnSum, columnIndex) => {
+        const cell: Cell = new Cell(rowIndex, columnIndex, rowSum + columnSum);
+        row.addCell(cell);
+    });
+
+    grid.addRow(row);
+});
+
+grid.checkReachableCells(T);
+
+const result: number = grid.countReachableCell();
+
+// console.log('answer');
+console.log(result);
