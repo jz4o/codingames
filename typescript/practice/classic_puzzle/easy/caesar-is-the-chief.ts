9b3f403bfbce3d251cf932955bd54be2b3726590
jz4o <26317360+jz4o@users.noreply.github.com>
2023-10-04 23:56:03 +0900

    add typescript/Caesar is the chief

---
+++ typescript/practice/classic_puzzle/easy/caesar-is-the-chief.ts
@@ -0,0 +1,31 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const textToDecode: string = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const ALPHABETS: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let words: string[] = textToDecode.split(' ');
+
+let result: string = 'WRONG MESSAGE';
+[...Array(ALPHABETS.length).keys()].some(() => {
+    if (words.includes('CHIEF')) {
+        result = words.join(' ');
+        return true;
+    }
+
+    words = words.map(word => {
+        return word
+            .split('')
+            .map(char => ALPHABETS.charAt((ALPHABETS.indexOf(char) || ALPHABETS.length) - 1))
+            .join('');
+    });
+});
+
+// console.log('DecodedText:string(101)');
+console.log(result);
