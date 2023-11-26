a4c758676ac22f1f822a19567d70cbff0bf7c75d
jz4o <26317360+jz4o@users.noreply.github.com>
2023-11-26 23:45:53 +0900

    add typescript/IPv6 Shortener

---
+++ typescript/practice/classic_puzzle/easy/ipv6-shortener.ts
@@ -0,0 +1,23 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const ip: string = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+const longestOmit: string = ip
+    .match(/(0000(:0000)+)/g)
+    ?.reduce((longestOmit, omit) => longestOmit.length < omit.length ? omit : longestOmit);
+
+const result: string = ip
+    .replace(longestOmit, ':')
+    .replace(':::', '::')
+    .replace(/^0+/g, '0')
+    .replace(/:0+/g, ':0')
+    .replace(/:0([1-9a-f])/g, ':$1');
+
+// console.log('output');
+console.log(result);
