b1e2c514b6c4be3f3138ddfe1b0f3836e677a24b
jz4o <26317360+jz4o@users.noreply.github.com>
2023-12-17 17:11:24 +0900

    add typescript/Ancestors & Descendants

---
+++ typescript/practice/classic_puzzle/medium/ancestors-descendants.ts
@@ -0,0 +1,42 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const count: number = parseInt(readline());
+const lines: string[] = [];
+for (let i = 0; i < count; i++) {
+    const line: string = readline();
+    lines.push(line);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+type Generation = {
+    level: number,
+    name: string
+};
+
+const generations: Generation[] = lines.map(line => {
+    const level: number = line.split('').filter(c => c === '.').length;
+    return { level: level, name: line.slice(level) };
+});
+
+const results: string[] = [];
+while (generations.length !== 0) {
+    const inheritances: string[] = [...Array(generations[generations.length - 1].level + 1).keys()]
+        .map(l => generations.filter(generation => generation.level === l).pop().name);
+
+    results.unshift(inheritances.join(' > '));
+
+    let lastGeneration: Generation = generations.pop();
+    while (generations.length > 0 && generations[generations.length - 1].level < lastGeneration.level) {
+        lastGeneration = generations.pop();
+    }
+}
+
+// console.log('answer');
+results.forEach(result => {
+    console.log(result);
+});
