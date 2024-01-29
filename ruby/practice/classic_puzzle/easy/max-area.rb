125fb4bf49c78ec5ef2f2875cccf9b74850531f8
jz4o <26317360+jz4o@users.noreply.github.com>
2024-01-29 23:56:42 +0900

    add ruby/Max Area

---
+++ ruby/practice/classic_puzzle/easy/max-area.rb
@@ -0,0 +1,27 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = gets.to_i
+inputs = gets.split
+as = []
+n.times do |i|
+  a = inputs[i].to_i
+  as << a
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+areas = as.flat_map.with_index do |l, l_index|
+  as[l_index + 1..].map.with_index(l_index + 1) do |r, r_index|
+    height = [l, r].min
+    width = r_index - l_index
+
+    height * width
+  end
+end
+
+result = areas.max
+
+# puts "answer"
+puts result
