941bfec7cef2698a88782d491cd14926d09b4d09
jz4o <26317360+jz4o@users.noreply.github.com>
2024-04-21 10:48:12 +0900

    add python3/abcdefghijklmnopqrstuvwxyz

---
+++ python3/practice/classic_puzzle/easy/abcdefghijklmnopqrstuvwxyz.py
@@ -0,0 +1,65 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+ms = []
+for i in range(n):
+    m = input()
+    ms.append(m)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+class Mass:
+    def __init__(self, row_index, column_index, value):
+        self.row_index = row_index
+        self.column_index = column_index
+        self.value = value
+
+
+mass_grid = []
+for row_index, row in enumerate(ms):
+    row = [Mass(row_index, column_index, value) for column_index, value in enumerate(list(row))]
+    mass_grid.append(row)
+
+candidate_mass_lists = [[mass] for mass in sum(mass_grid, []) if mass.value == 'a']
+
+result_list = []
+candidate_mass_list = []
+while len(candidate_mass_lists) > 0:
+    candidate_mass_list = candidate_mass_lists.pop(0)
+
+    last_mass = candidate_mass_list[-1]
+    if last_mass.value == 'z':
+        result_list = candidate_mass_list
+        break
+
+    target_value = chr(ord(last_mass.value) + 1)
+
+    around_last_masses = []
+    if last_mass.row_index > 0:
+        around_last_masses.append(mass_grid[last_mass.row_index - 1][last_mass.column_index])
+    if last_mass.column_index > 0:
+        around_last_masses.append(mass_grid[last_mass.row_index][last_mass.column_index - 1])
+    if last_mass.column_index + 1 < len(mass_grid[last_mass.row_index]):
+        around_last_masses.append(mass_grid[last_mass.row_index][last_mass.column_index + 1])
+    if last_mass.row_index + 1 < len(mass_grid):
+        around_last_masses.append(mass_grid[last_mass.row_index + 1][last_mass.column_index])
+
+    for mass in around_last_masses:
+        if mass.value == target_value:
+            candidate_mass_lists.append(candidate_mass_list + [mass])
+
+result_grid = [['-'] * n for _ in range(n)]
+for mass in result_list:
+    result_grid[mass.row_index][mass.column_index] = mass.value
+
+results = [''.join(row) for row in result_grid]
+
+# print("answer")
+for result in results:
+    print(result)
