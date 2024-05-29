7cae77a5c72c3cebc0c313ee3efb78f632fcf575
jz4o <26317360+jz4o@users.noreply.github.com>
2024-05-29 23:21:38 +0900

    add python3/Fibonacci's Rabbit

---
+++ python3/practice/classic_puzzle/easy/fibonaccis-rabbit.py
@@ -0,0 +1,25 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+f0, n = [int(i) for i in input().split()]
+a, b = [int(i) for i in input().split()]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+temp_list = [0] * n
+temp_list.insert(0, f0)
+
+for i in range(a, n + 1):
+    begin_index = max(i - b, 0)
+    end_index = i - a
+
+    temp_list[i] = sum(temp_list[begin_index:end_index + 1])
+
+result = temp_list[-1]
+
+# print("Rabbits on time N.")
+print(result)
