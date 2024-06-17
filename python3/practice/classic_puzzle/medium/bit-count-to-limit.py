5d878982422e10c10561651b8b2160f9aaf80eb6
jz4o <26317360+jz4o@users.noreply.github.com>
2024-06-17 23:15:27 +0900

    refactor python3/Bit count to limit

--- python3/practice/classic_puzzle/medium/bit-count-to-limit.py
+++ python3/practice/classic_puzzle/medium/bit-count-to-limit.py
@@ -11,14 +11,20 @@ n = int(input())
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-n += 1
 binary_length = len(np.base_repr(n))
 
-result = 0
-for i in range(1, binary_length + 1):
-    div, mod = divmod(n, 2**i)
+zero_include_n = n + 1
 
-    result += div * (2**(i - 1)) + max(0, mod - (2**(i - 1)))
+result = 0
+for i in range(binary_length):
+    zero_length_in_cycle = 2**i
+    one_length_in_cycle = 2**i
+
+    binary_cycle_length = zero_length_in_cycle + one_length_in_cycle
+
+    div, mod = divmod(zero_include_n, binary_cycle_length)
+
+    result += (div * one_length_in_cycle) + max(mod - zero_length_in_cycle, 0)
 
 # print("Number of 1s")
 print(result)
