0c69005b339c58b0b2b5051de18e3c9a751a842f
jz4o <26317360+jz4o@users.noreply.github.com>
2024-08-01 23:06:19 +0900

    refactor typescript/Pirate's treasure

--- typescript/practice/classic_puzzle/easy/pirates-treasure.ts
+++ typescript/practice/classic_puzzle/easy/pirates-treasure.ts
@@ -24,37 +24,35 @@ map.push(Array(W + 2).fill(1));
 // Write an action using console.log()
 // To debug: console.error('Debug messages...');
 
+const rangeArrayFromTo: (from: number, to: number, step?: number) => number[] = (from: number, to: number, step: number = 1): number[] => {
+    return [...Array(Math.floor((to - from + step) / step)).keys()].map(i => from + i * step);
+};
+
 let result: string;
-try {
-    for (let h = 1; h <= H; h++) {
-        for (let w = 1; w <= W; w++) {
-            if (map[h][w] !== 0) {
-                continue;
-            }
-
-            const aroundMasses: number[] = [];
-            aroundMasses.push(map[h - 1][w - 1]); // upper left
-            aroundMasses.push(map[h - 1][w]);     // upper
-            aroundMasses.push(map[h - 1][w + 1]); // upper right
-            aroundMasses.push(map[h][w - 1]);     // left
-            aroundMasses.push(map[h][w + 1]);     // right
-            aroundMasses.push(map[h + 1][w - 1]); // lower left
-            aroundMasses.push(map[h + 1][w]);     // under
-            aroundMasses.push(map[h + 1][w + 1]); // lower right
-            if (aroundMasses.some(mass => mass !== 1)) {
-                continue;
-            }
-
-            result = `${w - 1} ${h - 1}`;
-            throw false; // global escape
+rangeArrayFromTo(1, H).some(h => {
+    return rangeArrayFromTo(1, W).some(w => {
+        if (map[h][w] !== 0) {
+            return false;
         }
-    }
-} catch (_) {
-    // for global escape
-}
 
-// # Write an action using puts
-// # To debug: STDERR.puts "Debug messages..."
+        const aroundMasses: number[] = [];
+        aroundMasses.push(map[h - 1][w - 1]); // upper left
+        aroundMasses.push(map[h - 1][w]);     // upper
+        aroundMasses.push(map[h - 1][w + 1]); // upper right
+        aroundMasses.push(map[h][w - 1]);     // left
+        aroundMasses.push(map[h][w + 1]);     // right
+        aroundMasses.push(map[h + 1][w - 1]); // lower left
+        aroundMasses.push(map[h + 1][w]);     // under
+        aroundMasses.push(map[h + 1][w + 1]); // lower right
+        if (aroundMasses.some(mass => mass !== 1)) {
+            return false;
+        }
+
+        result = `${w - 1} ${h - 1}`;
+
+        return true;
+    });
+});
 
 // console.log('x y');
 console.log(result);
