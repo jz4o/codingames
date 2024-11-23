7f668364f0bc82732429293d3764d2dcfe406274
jz4o <26317360+jz4o@users.noreply.github.com>
2024-11-24 00:05:21 +0900

    fix ruff offence

--- python3/practice/classic_puzzle/easy/how-high-up-does-the-beer-go.py
+++ python3/practice/classic_puzzle/easy/how-high-up-does-the-beer-go.py
@@ -1,7 +1,7 @@
 # import sys
 # import math
 
-from math import pi, pow
+from math import pi
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
