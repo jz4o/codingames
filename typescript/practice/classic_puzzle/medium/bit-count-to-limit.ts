4981284a864c73488a3e8f2b84caf0c0ba71401a
jz4o <26317360+jz4o@users.noreply.github.com>
2025-05-06 10:56:51 +0900

    refactor typescript/Bit count to limit

--- typescript/practice/classic_puzzle/medium/bit-count-to-limit.ts
+++ typescript/practice/classic_puzzle/medium/bit-count-to-limit.ts
@@ -8,24 +8,19 @@ const n: number = parseInt(readline());
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
 
-const rangeArrayFromTo: (from: number, to: number, step?: number) => number[] = (from: number, to: number, step: number = 1): number[] => {
-    return [...Array(Math.floor((to - from + step) / step)).keys()].map(i => from + i * step);
-};
-
 const binaryLength: number = n.toString(2).length;
 
-const zeroIncludedN: number = n + 1;
+const kindOfNumber: number = n + 1;
 
-const result: string = rangeArrayFromTo(1, binaryLength)
-    .reduce((result, i) => {
-        const binaryCycleLength: number = Math.pow(2, i);
-        const zeroLengthInCycle: number = binaryCycleLength / 2;
-        const oneLengthInCycle: number = binaryCycleLength / 2;
+const result: string = [...Array(binaryLength).keys()]
+    .reduce((count, i) => {
+        const halfCycleLength: number = Math.pow(2, i);
+        const cycleLength: number = halfCycleLength * 2;
 
-        const div: number = Math.floor(zeroIncludedN / binaryCycleLength);
-        const mod: number = zeroIncludedN % binaryCycleLength;
+        const div: number = Math.floor(kindOfNumber / cycleLength);
+        const mod: number = kindOfNumber % cycleLength;
 
-        return result + (div * oneLengthInCycle) + Math.max(mod - zeroLengthInCycle, 0);
+        return count + (div * halfCycleLength) + Math.max(mod - halfCycleLength, 0);
     }, 0)
     .toString();
 
