99f7b0c4b9f94ecabb952c95e23a08adc3165046
jz4o <26317360+jz4o@users.noreply.github.com>
2026-06-22 23:05:00 +0900

    refactor ruby/MiniCPU Instruction Decoder

--- ruby/practice/classic_puzzle/easy/minicpu-instruction-decoder.rb
+++ ruby/practice/classic_puzzle/easy/minicpu-instruction-decoder.rb
@@ -5,65 +5,77 @@ program = gets.chomp # Space-separated hex bytes representing CPU instructions
 # Write an answer using puts
 # To debug: STDERR.puts "Debug messages..."
 
-class Instruction
-  attr_reader :code, :operation
+class MiniCpu
+  REGISTER_NUMBERS = %w[00 01 02 03].freeze
 
-  def initialize(code, operation)
-    @code = code
-    @operation = operation
+  def self.exec(program)
+    @register = REGISTER_NUMBERS.to_h { |key| [key, 0] }
+
+    bytes = program.split
+    while instruction = Instruction.from_code(bytes.shift)
+      break if instruction == Instruction::HLT
+
+      if [Instruction::INC, Instruction::DEC].include? instruction
+        register_number = bytes.shift
+        method(instruction.operation).call register_number
+      elsif [Instruction::MOV, Instruction::ADD, Instruction::SUB, Instruction::MUL].include? instruction
+        register_number, value = bytes.shift 2
+        value = value.to_i 16 if instruction == Instruction::MOV
+
+        method(instruction.operation).call register_number, value
+      end
+    end
+
+    @register.transform_values { |value| value % 256 }.values_at(*REGISTER_NUMBERS)
   end
 
-  MOV = new '01', lambda { |register, register_number, value|
-    register[register_number] = value
-  }
-  ADD = new '02', lambda { |register, register_number1, register_number2|
-    register[register_number1] += register[register_number2]
-  }
-  SUB = new '03', lambda { |register, register_number1, register_number2|
-    register[register_number1] -= register[register_number2]
-  }
-  MUL = new '04', lambda { |register, register_number1, register_number2|
-    register[register_number1] *= register[register_number2]
-  }
-  INC = new '05', ->(register, register_number) { register[register_number] += 1 }
-  DEC = new '06', ->(register, register_number) { register[register_number] -= 1 }
-  HLT = new 'FF', nil
+  def self.mov(register_number, value)
+    @register[register_number] = value
+  end
 
-  ONE_ARGS = [INC, DEC].freeze
-  TWO_ARGS = [MOV, ADD, SUB, MUL].freeze
+  def self.add(register_number1, register_number2)
+    @register[register_number1] += @register[register_number2]
+  end
 
-  def self.from_code(code)
-    [MOV, ADD, SUB, MUL, INC, DEC, HLT].find { |instruction| instruction.code == code }
+  def self.sub(register_number1, register_number2)
+    @register[register_number1] -= @register[register_number2]
+  end
+
+  def self.mul(register_number1, register_number2)
+    @register[register_number1] *= @register[register_number2]
+  end
+
+  def self.inc(register_number)
+    @register[register_number] += 1
+  end
+
+  def self.dec(register_number)
+    @register[register_number] -= 1
+  end
+
+  class Instruction
+    attr_reader :code, :operation
+
+    def initialize(code, operation)
+      @code = code
+      @operation = operation
+    end
+
+    MOV = new '01', :mov
+    ADD = new '02', :add
+    SUB = new '03', :sub
+    MUL = new '04', :mul
+    INC = new '05', :inc
+    DEC = new '06', :dec
+    HLT = new 'FF', nil
+
+    def self.from_code(code)
+      [MOV, ADD, SUB, MUL, INC, DEC, HLT].find { |instruction| instruction.code == code }
+    end
   end
 end
 
-register = {
-  '00' => 0,
-  '01' => 0,
-  '02' => 0,
-  '03' => 0
-}
-bytes = program.split
-while instruction = Instruction.from_code(bytes.shift)
-  break if instruction == Instruction::HLT
-
-  if Instruction::ONE_ARGS.include? instruction
-    register_number = bytes.shift
-    instruction.operation.call(register, register_number)
-  elsif Instruction::TWO_ARGS.include? instruction
-    register_number, value = bytes.shift 2
-    value = value.to_i(16) if instruction == Instruction::MOV
-
-    instruction.operation.call(register, register_number, value)
-  end
-end
-
-results = [
-  register['00'] % 256,
-  register['01'] % 256,
-  register['02'] % 256,
-  register['03'] % 256
-]
+results = MiniCpu.exec program
 
 # Print the final value of each register R0, R1, R2, R3, one value per line
 # puts "answer"
